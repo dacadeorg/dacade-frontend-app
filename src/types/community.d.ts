d635eb0b2d4d0b001c3fe111f0d0817bdf510789
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-21 13:47:14 +0200

    feat: add community and course type definition

---
+++ src/types/community.d.ts
@@ -0,0 +1,26 @@
+import { Metadata, Reward } from "./course";
+
+export interface Community {
+  id: string;
+  ref: string;
+  created_at: Date;
+  updated_at: Date;
+  summary: string;
+  icon: string;
+  name: string;
+  colors: Colors;
+  slug: string;
+  active: boolean;
+  description: string;
+  metadata: Omit<Metadata, "bestSubmissions">;
+  timestamp: number;
+  rewards: Reward[];
+  courses: number;
+}
+
+export interface Colors {
+  textAccent: string;
+  text: string;
+  accent: string;
+  primary: string;
+}
