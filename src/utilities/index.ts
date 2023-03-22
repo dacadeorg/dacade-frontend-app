dd84ab004867294a3cea705ea2e513acecd78fc2
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-22 11:19:51 +0200

    fix: improve the  type definition

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,9 +1,10 @@
 /**
  * Provide duration in milimiter per second.
  * @date 2023-03-22
- * @param {any} ms:number
- * @returns {any}
+ * @param {number} ms
+ * @returns {Promise<unknown>}
  */
-export function sleep(ms: number) {
+
+export function sleep(ms: number): Promise<unknown> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
