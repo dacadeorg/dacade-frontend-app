1d0d6345b0dac826d446adf181dca191800dac60
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-22 20:00:46 +0200

    chore: add webpack loader for SVGR

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
-  reactStrictMode: true,
-  i18n
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  reactStrictMode: true,
+  i18n,
+};
+
+module.exports = nextConfig;
