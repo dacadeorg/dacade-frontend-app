3974d1e2b953040f92016cc6e5e91d0946c9802b
Samantha <mureraksamantha@gmail.com>
2023-03-22 17:42:14 +0200

    refact: correct typo in classname from test to text

--- src/components/ui/EmptyState.tsx
+++ src/components/ui/EmptyState.tsx
@@ -9,7 +9,7 @@ function EmptyState({ title, subtitle }: EmptyStateProps): ReactElement {
   return (
     <div className="w-full text-center text-gray-500 py-20 text-base">
       {title && <h1 className="text-3xl mb-5">{title}</h1>}
-      {subtitle && <p className="test-sm" dangerouslySetInnerHTML={{ __html: subtitle }} />}
+      {subtitle && <p className="text-sm" dangerouslySetInnerHTML={{ __html: subtitle }} />}
     </div>
   );
 };
