93f75da2d9d12569dd7f7ac63226548124eab246
mbukeRepo <mbuke404@gmail.com>
2023-03-22 17:34:08 +0200

    feat: add comment

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -1,17 +1,27 @@
 import Link from "next/link";
-import React, { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
+/**
+ * Interface for NavItem component props
+ * @date 3/22/2023 - 5:33:20 PM
+ *
+ * @interface NavItemProps
+ * @typedef {NavItemProps}
+ */
 interface NavItemProps {
   type?: string;
   to?: string;
   children?: ReactNode;
 }
 
-const NavItem: React.FC<NavItemProps> = ({
-  type = "item",
-  to = "/",
-  children,
-}) => {
+/**
+ *  NavItem component
+ * @date 3/22/2023 - 5:33:34 PM
+ *
+ * @param {{ type?: string; to?: string; children: any; }} { type = "item", to = "/", children }
+ * @returns {JSX}
+ */
+const NavItem: FC<NavItemProps> = ({ type = "item", to = "/", children }) => {
   return (
     <li className={`nav-${type}`}>
       <Link href={to}>{children}</Link>
