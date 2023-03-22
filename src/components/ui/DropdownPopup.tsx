9ecd376f576cdb79969b7dbb91b2f7883e5dc4e9
Ntwali Edson <ntwaliedson9@gmail.com>
2023-03-22 17:02:44 +0200

    feat: migrated dropdown poppup

---
+++ src/components/ui/DropdownPopup.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+interface DropdownPopupProps {
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+export default function DropdownPopup({
+  onClose,
+  children,
+}: DropdownPopupProps) {
+  const toggleInvite = () => {
+    onClose();
+  };
+  return (
+    <div
+      style={{
+        width: "calc(100vw - 40px)",
+        maxWidth: "340px",
+        maxHeight: "calc(100vh - 100px)",
+        overflow: "hidden scroll",
+      }}
+      className="absolute top-14 right-0 z-40 bg-white rounded-3.5xl no-scrollbar text-gray-900"
+    >
+      {children}
+    </div>
+  );
+}
