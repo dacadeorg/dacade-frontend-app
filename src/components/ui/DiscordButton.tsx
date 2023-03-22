a5d0c6cb2c11ddff41e174a7ae8f2ce89ba0fd84
Ntwali Edson <ntwaliedson9@gmail.com>
2023-03-22 15:38:15 +0200

    feat: migrated discord button

---
+++ src/components/ui/DiscordButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+
+export default function DiscordButton() {
+  return (
+    <a
+      className="fixed bottom-5 right-5 z-999 md:w-16 w-14 cursor-pointer"
+      href="https://discord.gg/eHYZr9dzan"
+      target="_blank"
+    >
+      <Image
+        priority
+        unoptimized
+        fill
+        src="/assets/icons/discordButton.svg"
+        alt="Discord"
+      />
+    </a>
+  );
+}
