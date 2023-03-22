372d0a1fad080b4f60a60b7e906e09084e1f95d0
serapieTuyishime <tuyiserapie1@gmail.com>
2023-03-22 17:25:37 +0200

    feat: migrate section component from the ui folder

---
+++ src/components/ui/Section.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from "react";
+
+type Props = {
+    type?: string;
+    padding?: string;
+};
+
+function Section({ type = "default", padding = "py-4" }: Props): ReactElement {
+    return (
+        <div
+            className={`relative ${
+                type === "default"
+                    ? "bg-white text-gray-900"
+                    : type === "primary"
+                    ? "bg-primary text-white"
+                    : type === "secondary"
+                    ? "bg-secondary text-gray-900"
+                    : "bg-gray-50 text-gray-900"
+            }`}
+        >
+            <div className={padding}>
+                <slot />
+            </div>
+        </div>
+    );
+}
+
+export default Section;
