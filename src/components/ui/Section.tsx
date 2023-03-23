a9722624cd087cb99a0d6adde26375cf5144f5c6
mbukeRepo <mbuke404@gmail.com>
2023-03-23 20:37:11 +0200

    fix: fixing the build errors

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,32 +1,46 @@
 import { ReactElement } from "react";
 import classNames from "classnames";
 
+/**
+ * Section component props
+ * @date 3/23/2023 - 8:35:39 PM
+ *
+ * @interface Props
+ * @typedef {Props}
+ */
 interface Props {
-    type?: string;
-    padding?: string;
-    children: ReactElement;
+  type?: string;
+  padding?: string;
+  children: ReactElement;
 }
 
-
-
+/**
+ * Section component
+ * @date 3/23/2023 - 8:35:24 PM
+ *
+ * @export
+ * @param {Props} {
+  type = "default",
+  padding = "py-4",
+  children,
+}
+ * @returns {ReactElement}
+ */
 export default function Section({
-    type = "default",
-    padding = "py-4",
-    children,
+  type = "default",
+  padding = "py-4",
+  children,
 }: Props): ReactElement {
+  const sectionClassName = classNames("relative", {
+    "bg-white text-gray-900": type === "default",
+    "bg-primary text-white": type === "primary",
+    "bg-secondary text-gray-900": type === "secondary",
+    "bg-gray-50 text-gray-900": !type,
+  });
 
-    const sectionClassName = classNames("relative", {
-        "bg-white text-gray-900": type === "default",
-        "bg-primary text-white": type === "primary",
-        "bg-secondary text-gray-900": type === "secondary",
-        "bg-gray-50 text-gray-900": !type
-      });
-
-    return (
-        <div
-            className={sectionClassNames}
-        >
-            <div className={padding}>{children}</div>
-        </div>
-    );
+  return (
+    <div className={sectionClassName}>
+      <div className={padding}>{children}</div>
+    </div>
+  );
 }
