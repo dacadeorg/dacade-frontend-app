9c57c94b81e57f2ac752220d7ae6dd0ed43251b4
Ntwali Edson <ntwaliedson9@gmail.com>
2023-03-23 18:23:05 +0200

    feat: implemented svgr & comments

--- src/components/ui/DiscordButton.tsx
+++ src/components/ui/DiscordButton.tsx
@@ -1,19 +1,20 @@
-import Image from "next/image";
+import DiscordButtonIcon from "../../../public/assets/icons/discordButton.svg";
 
+/**
+ * Discord Button Component
+ * @date 3/23/2023 - 6:21:06 PM
+ *
+ * @export
+ * @returns {*}
+ */
 export default function DiscordButton() {
   return (
     <a
-      className="fixed bottom-5 right-5 z-999 md:w-16 w-14 cursor-pointer"
+      className="fixed- bottom-5 right-5 z-999 md:w-16 w-14 cursor-pointer"
       href="https://discord.gg/eHYZr9dzan"
       target="_blank"
     >
-      <Image
-        priority
-        unoptimized
-        fill
-        src="/assets/icons/discordButton.svg"
-        alt="Discord"
-      />
+      <DiscordButtonIcon />
     </a>
   );
 }
