159bff7887c5eaf34fd0e89b63f4610b97333d23
Ntwali Edson <ntwaliedson9@gmail.com>
2023-03-23 17:17:42 +0200

    feat: implemented svgr and requested changes

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,37 +1,51 @@
 import classNames from "classnames";
-import Image from "next/image";
+import Spinner from "../../../public/assets/icons/spinner.svg";
 
+/**
+ * Loader Props Interface
+ * @date 3/23/2023 - 5:09:01 PM
+ *
+ * @interface LoaderProps
+ * @typedef {LoaderProps}
+ */
 interface LoaderProps {
   communityStyles?: boolean;
 }
-export default function Loader({ communityStyles = false }: LoaderProps) {
-  const colors = {
-    text: "#0D61FF",
-    accent: "#0D61FF",
-    textAccent: "#fff",
-    primary: "#0D61FF",
-  };
 
+/**
+ * Loader Component
+ * @date 3/23/2023 - 5:09:34 PM
+ *
+ * @export
+ * @param {LoaderProps} { communityStyles = false }
+ * @returns {*}
+ */
+const colors = {
+  text: "#0D61FF",
+  accent: "#0D61FF",
+  textAccent: "#fff",
+  primary: "#0D61FF",
+};
+
+export default function Loader({
+  communityStyles = false,
+}: LoaderProps) {
   const styles = {
     color: colors.textAccent,
   };
+
+  const loaderClasses = classNames(
+    "flex items-center relative justify-center !h-12 !w-12",
+    {
+      "text-primary": !communityStyles,
+    }
+  );
   return (
     <div
-      className={classNames(
-        "flex items-center relative justify-center !h-12 !w-12",
-        {
-          "text-primary": !communityStyles,
-        }
-      )}
+      className={loaderClasses}
       style={{ ...(communityStyles ? styles : {}) }}
     >
-      <Image
-        src="/assets/icons/spinner.svg"
-        height={18}
-        width={18}
-        alt="loading"
-        className="animate-spin"
-      />
+      <Spinner height={18} width={18} className="animate-spin" />
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useTranslation } from "next-i18next";
 import { GetStaticProps } from "next";
 import i18Translate from "@/utilities/I18Translate";
+import Loader from "@/components/ui/Loader";
 
 export const getStaticProps: GetStaticProps = async ({ locale }) =>
   i18Translate(locale as string);
@@ -13,12 +14,16 @@ const Home = () => {
     <>
       <Head>
         <title>Dacade</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
         <meta name="description" content="Peer to peer learning" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-blue-500 text-white font-bold text-6xl flex justify-center items-center">
         <span>{t("nav.signup.already-exist")}</span>
+        <Loader />
       </main>
     </>
   );
