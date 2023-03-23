2216c0b278267f45e48bdd8118d994125fe2f98c
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-03-23 10:22:36 +0200

    refact: adds more descriptive comments

--- src/components/ui/Video.tsx
+++ src/components/ui/Video.tsx
@@ -1,17 +1,27 @@
+import { ReactElement } from "react";
+
+
 /**
- * Video component
- * This component is responsible for rendering videos using an iframe.
+ * Interface for video component props
+ * @date 3/23/2023 - 10:20:04 AM
  *
- * @date 3/22/2023 - 4:03:47 PM
- *
+ * @interface VideoProps
  * @typedef {VideoProps}
  */
-
-type VideoProps = {
+interface VideoProps {
   url: string;
 };
 
-export default function Video({ url = "" }: VideoProps) {
+/**
+ * Video component
+ * This component is responsible for rendering videos using an iframe.
+ * @date 3/23/2023 - 10:19:00 AM
+ *
+ * @export
+ * @param {VideoProps} { url }
+ * @returns {ReactElement}
+ */
+export default function Video({ url }: VideoProps): ReactElement {
   return (
     <div className="relative mt-7 lg:w-full lg:h-auto aspect-w-16 aspect-h-11 md:aspect-h-10 lg:aspect-h-9 md:w-full h-auto md:m-auto md:ml-auto">
       <iframe src={url} allowFullScreen />
