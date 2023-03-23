633c99b9073293d2899494541e34ebdcb8cfced2
Ntwali Edson <ntwaliedson9@gmail.com>
2023-03-23 12:22:04 +0200

    Update Tag.tsx

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 interface TagProps {
   rounded?: boolean;
   type?: "transparent" | "gray" | "light-gray" | "warn";
-  value?: string;
+  value: string;
   children?: React.ReactNode;
 }
 
@@ -13,18 +13,17 @@ export default function Tag({
   value,
   children,
 }: TagProps) {
+  const tagClassNames = classNames(
+    "text-center px-2 p-0.5 h-5 inline-flex items-center",
+    { "bg-transparent bg-opacity-25 text-white": type == "transparent" },
+    { "bg-gray-200 text-gray-500": type == "gray" },
+    { "bg-gray-100 text-gray-500": type == "light-gray" },
+    { "bg-yellow-100 text-yellow-900": type == "warn" },
+    { "rounded-full": rounded },
+    { rounded: !rounded }
+  );
   return (
-    <div
-      className={classNames(
-        "text-center px-2 p-0.5 h-5 inline-flex items-center",
-        { "bg-transparent bg-opacity-25 text-white": type == "transparent" },
-        { "bg-gray-200 text-gray-500": type == "gray" },
-        { "bg-gray-100 text-gray-500": type == "light-gray" },
-        { "bg-yellow-100 text-yellow-900": type == "warn" },
-        { "rounded-full": rounded },
-        { rounded: !rounded }
-      )}
-    >
+    <div className={tagClassNames}>
       <span className="text-xs font-medium block leading-none">
         {value ? <span>{value}</span> : children}
       </span>
