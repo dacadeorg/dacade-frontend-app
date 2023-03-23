767bd7e705ef284427e3b8c0e7294c9de10696eb
Ntwali Edson <ntwaliedson9@gmail.com>
2023-03-23 18:29:42 +0200

    feat: added comments

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,5 +1,12 @@
 import classNames from "classnames";
 
+/**
+ * Tag Props Interface
+ * @date 3/23/2023 - 6:28:57 PM
+ *
+ * @interface TagProps
+ * @typedef {TagProps}
+ */
 interface TagProps {
   rounded?: boolean;
   type?: "transparent" | "gray" | "light-gray" | "warn";
@@ -7,6 +14,19 @@ interface TagProps {
   children?: React.ReactNode;
 }
 
+/**
+ * Tag Component
+ * @date 3/23/2023 - 6:29:20 PM
+ *
+ * @export
+ * @param {TagProps} {
+  rounded = true,
+  type = "gray",
+  value,
+  children,
+}
+ * @returns {*}
+ */
 export default function Tag({
   rounded = true,
   type = "gray",
@@ -15,7 +35,10 @@ export default function Tag({
 }: TagProps) {
   const tagClassNames = classNames(
     "text-center px-2 p-0.5 h-5 inline-flex items-center",
-    { "bg-transparent bg-opacity-25 text-white": type == "transparent" },
+    {
+      "bg-transparent bg-opacity-25 text-white":
+        type == "transparent",
+    },
     { "bg-gray-200 text-gray-500": type == "gray" },
     { "bg-gray-100 text-gray-500": type == "light-gray" },
     { "bg-yellow-100 text-yellow-900": type == "warn" },
