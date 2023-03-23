437455705b4385efa6069d73a5cfaec4d3af2b39
mbukeRepo <mbuke404@gmail.com>
2023-03-23 15:40:11 +0200

    feat: apply changes

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useState, ChangeEvent, ReactElement } from "react";
+import { useState, ChangeEvent, ReactElement, useMemo } from "react";
 
 /**
  * Interface for TextInput component props
@@ -19,6 +19,8 @@ interface TextInputProps {
   handleInput?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
+
+
 /**
  * TextInput component
  * @date 3/22/2023 - 4:31:51 PM
@@ -37,23 +39,30 @@ export default function TextInput({
 }: TextInputProps): ReactElement {
   const [isFocused, setIsFocused] = useState(false);
 
-  const isFilled = value.trim().length > 0;
+  const isFilled = useMemo(() => (value.trim().length > 0), [value]);
+
+  const labelClassName = classNames(
+    "absolute top-0 left-0 text-lg px-5 py-5 z-10 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out items-center",
+    {
+      "text-gray-400 flex items-center": !isFilled && !isFocused,
+      "text-gray-400 scale-75 -translate-y-3 translate-x-1":
+        isFocused || isFilled,
+      "text-red-600": error,
+      "text-blue-500": isFocused && !error,
+    }
+  )
+
+  const textareaClassName = classNames(
+    "w-full border border-solid border-gray-200 h-56 resize-none m-0 pt-5 md:pt-7.5 pl-15 pr-2 md:px-10.75 block text-lg focus:outline-none placeholder-gray-400 placeholder-opacity-100",
+    inputClass
+  );
 
   return (
     <div>
       <div className="floating-input relative">
         {label && (
           <label
-            className={classNames(
-              "absolute top-0 left-0 text-lg px-5 py-5 z-10 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out items-center",
-              {
-                "text-gray-400 flex items-center": !isFilled && !isFocused,
-                "text-gray-400 scale-75 -translate-y-3 translate-x-1":
-                  isFocused || isFilled,
-                "text-red-600": error,
-                "text-blue-500": isFocused && !error,
-              }
-            )}
+            className={labelClassName}
           >
             {label}
           </label>
@@ -63,10 +72,7 @@ export default function TextInput({
           placeholder={placeholder}
           autoComplete="off"
           disabled={disabled}
-          className={classNames(
-            "w-full border border-solid border-gray-200 h-56 resize-none m-0 pt-5 md:pt-7.5 pl-15 pr-2 md:px-10.75 block text-lg focus:outline-none placeholder-gray-400 placeholder-opacity-100",
-            inputClass
-          )}
+          className={textareaClassName}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           onChange={handleInput}
