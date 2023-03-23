f3b365a8a97399017c239403aac54a85568eff0b
Samantha <mureraksamantha@gmail.com>
2023-03-23 12:38:59 +0200

    refact: use classnames package using the classNames

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,25 +1,30 @@
 import { ReactElement } from "react";
+import classNames from "classnames";
+
 interface Props {
     type?: string;
     padding?: string;
     children: ReactElement;
 }
+
+
+
 export default function Section({
     type = "default",
     padding = "py-4",
     children,
 }: Props): ReactElement {
+
+    const sectionClassNames = classNames("relative", {
+        "bg-white text-gray-900": type === "default",
+        "bg-primary text-white": type === "primary",
+        "bg-secondary text-gray-900": type === "secondary",
+        "bg-gray-50 text-gray-900": !type
+      });
+
     return (
         <div
-            className={`relative ${
-                type === "default"
-                    ? "bg-white text-gray-900"
-                    : type === "primary"
-                    ? "bg-primary text-white"
-                    : type === "secondary"
-                    ? "bg-secondary text-gray-900"
-                    : "bg-gray-50 text-gray-900"
-            }`}
+            className={sectionClassNames}
         >
             <div className={padding}>{children}</div>
         </div>
