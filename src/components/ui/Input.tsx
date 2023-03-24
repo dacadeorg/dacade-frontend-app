772fb9f6a079a8b9fd5f7a4b5c66577b5d72807d
Samantha <mureraksamantha@gmail.com>
2023-03-24 13:45:56 +0200

    refactor: use classname package for the classNames add comment for the interface and for the component apply break after every case in the switch statement

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,96 +1,134 @@
-import { useState } from "react";
+import { ChangeEvent, ReactElement, useMemo, useState } from "react";
+import classNames from "classnames";
 
-interface Props {
-    type?: string;
-    value?: string | number;
-    label?: string;
-    disabled?: boolean;
-    placeholder?: string;
-    error?: string;
-    inputClass?: string;
-    fontSize?: string;
-    onInput?: (value: string) => void;
+/**
+ * Interface for input component props
+ * @date 3/24/2023 - 1:09:06 PM
+ *
+ * @interface Props
+ * @typedef {Props}
+ */
+interface InputProps {
+  type?: string;
+  value?: string | number;
+  label?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  error?: string;
+  inputClass?: string;
+  fontSize?: string;
+  onInput?: (value: string) => void;
 }
 
-const Input: React.FC<Props> = ({
-    type = "text",
-    value = "",
-    label = null,
-    disabled = false,
-    placeholder = null!,
-    error = null,
-    inputClass = null,
-    fontSize = "lg",
-    onInput,
-}) => {
-    const [isFocused, setIsFocused] = useState(false);
 
-    const isFilled = String(value)?.trim().length > 0;
+/**
+ * Component for a customized input
+ * @date 3/24/2023 - 1:40:02 PM
+ *
+ * @export
+ * @param {InputProps} {
+  type = "text",
+  value = "",
+  label,
+  disabled = false,
+  placeholder = null!,
+  error,
+  inputClass,
+  fontSize = "lg",
+  onInput,
+}
+ * @returns {ReactElement}
+ */
+export default function Input({
+  type = "text",
+  value = "",
+  label,
+  disabled = false,
+  placeholder = null!,
+  error,
+  inputClass,
+  fontSize = "lg",
+  onInput,
+}: InputProps): ReactElement {
+  const [isFocused, setIsFocused] = useState(false);
 
-    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (onInput) {
-            onInput(event.target.value);
-        }
-    };
+  const isFilled = useMemo(() => String(value)?.trim().length > 0,[]);
 
-    const fontSizeClasses = () => {
-        switch (fontSize) {
-            case "2xl":
-            case "xl":
-            case "lg":
-            case "sm":
-            case "xs":
-                return `text-${fontSize}`;
-            default:
-                return "text-base";
-        }
-    };
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    if (onInput) {
+      onInput(event.target.value);
+    }
+  };
 
-    return (
-        <div className="relative">
-            <div
-                className={`relative dac-input ${
-                    label ? "floating-input" : ""
-                } ${isFilled || isFocused ? "floating-input" : ""}`}
-            >
-                {label && (
-                    <label
-                        className={`text-gray-400 flex items-center ${
-                            !isFilled && !isFocused
-                                ? "text-gray-400 flex items-center"
-                                : "text-gray-400 scale-75 -translate-y-3 translate-x-1"
-                        } ${error ? "text-red-600" : ""} ${
-                            isFocused && !error ? "text-blue-500" : ""
-                        } absolute top-0 left-0 text-lg px-5 py-5 z-10 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out items-center`}
-                    >
-                        {label}
-                    </label>
-                )}
-                <input
-                    className={`rounded-md focus:outline-none focus:shadow-sm w-full ${
-                        disabled
-                            ? "text-gray-400 scale-75 -translate-y-3 translate-x-1 bg-gray-50"
-                            : ""
-                    } ${error ? "border-red-100 rounded-b-none" : ""} ${
-                        !error ? "focus:border-gray-200 border-gray-200" : ""
-                    } ${inputClass} ${fontSizeClasses()}`}
-                    value={value}
-                    type={type}
-                    placeholder={placeholder}
-                    autoComplete="off"
-                    disabled={disabled}
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                    onInput={handleInput}
-                />
-            </div>
-            {error && (
-                <div className="bg-red-50 help text-sm rounded-b-md border border-t-0 border-red-100 text-red-900 px-5 py-1.5">
-                    <p>{error}</p>
-                </div>
-            )}
+  const fontSizeClasses = () => {
+    switch (fontSize) {
+      case "2xl":
+        break;
+      case "xl":
+        break;
+      case "lg":
+        break;
+      case "sm":
+        break;
+      case "xs":
+        return `text-${fontSize}`;
+      default:
+        return "text-base";
+    }
+  };
+
+  const labelClasssName = classNames(
+    "text-gray-400",
+    "flex",
+    "items-center",
+    {
+      "text-gray-400 scale-75 -translate-y-3 translate-x-1":
+        isFilled || isFocused,
+      "text-gray-400 flex items-center": !isFilled && !isFocused,
+      "text-red-600": error,
+      "text-blue-500": isFocused && !error,
+      "absolute top-0 left-0 text-lg px-5 py-5 z-10 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out items-center":
+        true,
+    }
+  );
+
+  const inputComponentClassName = classNames("relative dac-input", {
+    "floating-input": label || isFilled || isFocused,
+  });
+
+  const inputClassName = classNames(
+    `rounded-md focus:outline-none focus:shadow-sm w-full ${inputClass} ${fontSizeClasses()}`,
+    {
+      "text-gray-400 scale-75 -translate-y-3 translate-x-1 bg-gray-50":
+        disabled,
+      "border-red-100 rounded-b-none": error,
+      "focus:border-gray-200 border-gray-200": !error,
+    }
+  );
+
+  return (
+    <div className="relative">
+      <div
+        className={inputComponentClassName}
+      >
+        {label && <label className={labelClasssName}>{label}</label>}
+        <input
+          className={inputClassName}
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          autoComplete="off"
+          disabled={disabled}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onInput={handleInput}
+        />
+      </div>
+      {error && (
+        <div className="bg-red-50 help text-sm rounded-b-md border border-t-0 border-red-100 text-red-900 px-5 py-1.5">
+          <p>{error}</p>
         </div>
-    );
-};
-
-export default Input;
+      )}
+    </div>
+  );
+}
