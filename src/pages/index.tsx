9a9f0b209bc541eec79458c56bd1d790a4ebbf97
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-24 13:30:53 +0200

    refactor: remove unnecessary component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "next-i18next";
 import { GetStaticProps } from "next";
 import i18Translate from "@/utilities/I18Translate";
 import Loader from "@/components/ui/Loader";
-
+import MarkdownIcon from "@/components/ui/MarkdownIcon";
 
 export const getStaticProps: GetStaticProps = async ({ locale }) =>
   i18Translate(locale as string);
@@ -23,7 +23,6 @@ const Home = () => {
       </Head>
       <main className="bg-blue-500 text-white font-bold text-6xl flex justify-center items-center">
         <span>{t("nav.signup.already-exist")}</span>
-        <Loader />
       </main>
     </>
   );
