8dfefdf99a91d7e76fb6096e2049a3c38a30a399
Samantha <mureraksamantha@gmail.com>
2023-03-27 18:41:51 +0200

    feature: add wrapper folder in the components folder after migration

---
+++ src/components/wrappers/ThemeWrapper.tsx
@@ -0,0 +1,68 @@
+import { CSSProperties, ReactElement, useMemo } from "react";
+
+
+/**
+ * Theme wrapper property interface
+ * @date 3/27/2023 - 6:39:31 PM
+ *
+ * @interface ThemeWrapperProps
+ * @typedef {ThemeWrapperProps}
+ */
+interface ThemeWrapperProps {
+  colors?: {
+    primary?: string;
+    secondary?: string;
+    text?: string;
+    highlight?: string;
+    accent?: string;
+    muted?: string;
+  };
+  children: React.ReactNode;
+}
+
+
+/**
+ * Interface for the css style properties
+ * @date 3/27/2023 - 6:40:04 PM
+ *
+ * @interface CSSVars
+ * @typedef {CSSVars}
+ * @extends {CSSProperties}
+ */
+interface CSSVars extends CSSProperties {
+    '--tm-primary'?: string,
+      '--tm-secondary'?: string,
+      '--tm-text'?: string,
+      '--tm-highlight'?: string,
+      '--tm-accent'?: string,
+      '--tm-muted'?: string
+}
+
+
+
+/**
+ * Theme wrapper components
+ * @date 3/27/2023 - 6:40:41 PM
+ *
+ * @export
+ * @param {ThemeWrapperProps} { colors = {}, children }
+ * @returns {ReactElement}
+ */
+export default function ThemeWrapper ({ colors = {}, children }: ThemeWrapperProps): ReactElement {
+  const cssVars: CSSVars = useMemo(() => {
+    return {
+      '--tm-primary': colors.primary,
+      '--tm-secondary': colors.secondary,
+      '--tm-text': colors.text,
+      '--tm-highlight': colors.highlight,
+      '--tm-accent': colors.accent,
+      '--tm-muted': colors.muted,
+    };
+  }, [colors]);
+
+  return (
+    <div style={cssVars}>
+      {children}
+    </div>
+  );
+};
