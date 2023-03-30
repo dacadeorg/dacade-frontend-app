91559f77529599eac787a584aab8243c2e19034b
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-30 10:36:54 +0200

    feat: migrate Scoreboard list component

---
+++ src/components/list/Scoreboard.tsx
@@ -0,0 +1,92 @@
+// TODO: Should uncommented when redux is implemented
+// import { useSelector } from "react-redux";
+import Avatar from "@/components/ui/Avatar";
+import Badge from "@/components/ui/Badge";
+import { useTranslation } from "next-i18next";
+import { ReactElement } from "react";
+
+// TODO: This is a temporary user interface solution.
+// it should updated when the user types is added
+interface User {
+  key: string;
+  reputation: number;
+  displayName: string;
+  feedbacks: number;
+}
+
+/**
+ * Scoreboard list props interface
+ * @date 3/30/2023 - 10:30:58 AM
+ *
+ * @interface ScoreboardListProps
+ * @typedef {ScoreboardListProps}
+ */
+interface ScoreboardListProps {
+  badgeStyles?: Record<string, string>;
+  users?: User[];
+}
+
+/**
+ * Scoreboard list component
+ * @date 3/30/2023 - 10:31:19 AM
+ *
+ * @export
+ * @param {ScoreboardListProps} {
+  badgeStyles = { backgroundColor: "purple" },
+  users = [],
+}
+ * @returns {ReactElement}
+ */
+
+export default function ScoreboardList({
+  badgeStyles = { backgroundColor: "purple" },
+  users = [],
+}: ScoreboardListProps): ReactElement {
+  const { t } = useTranslation();
+
+  // TODO: Should uncommented when redux is implemented
+  //   const colors = useSelector((state: RootState) => state.ui.colors);
+  //   const community = useSelector(
+  //     (state: RootState) => state.communities.current
+  //   );
+
+  return (
+    <div className="pb-5 w-full divide-y divide-gray-200 space-y-4 bg-gray-50 rounded-3xl lg:max-w-2xl">
+      {users.map((user) => (
+        <div key={user.key} className="px-3">
+          <div className="h-36 md:h-24 md:flex w-full justify-between md:justify-end space-x-5 space-y-0 pt-6 md:flex-row-reverse md:pt-6">
+            <div className="relative flex flex-row-reverse md:w-full md:justify-items-stretch md:relative md:text-right md:right-0">
+              <div className="absolute md:relative md:top-0 -bottom-28 md:-bottom-24 font-medium text-gray-500 left-5 md:left-auto md:right-5 px-2 py-1 h-6 text-xs bg-gray-200 rounded-full">
+                {user.reputation}
+              </div>
+            </div>
+            <div className="w-full md:w-auto">
+              <div className="text-lg font-medium">
+                {user.displayName}
+              </div>
+              <div className="whitespace-nowrap text-base">
+                <span className="font-medium">
+                  {user.feedbacks}
+                  <span className="font-normal">
+                    {t("feedbacks")}
+                  </span>
+                </span>
+              </div>
+            </div>
+
+            <div className="relative">
+              <div className="absolute md:relative right-2 md:-bottom-10 -bottom-14 md:top-0">
+                <Avatar user={user} size="large" />
+                <Badge
+                  className="absolute left-9 top-10 w-6 h-6"
+                  value="1"
+                  customStyle={badgeStyles}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
