53a154a73348562302e66be56a60fe72c37b25a4
mbukerepo <mbuke404@gmail.com>
2023-04-03 12:46:43 +0200

    fix: unblock SubmissionView component

--- src/components/cards/SubmissionView.tsx
+++ src/components/cards/SubmissionView.tsx
@@ -2,38 +2,54 @@
 import { useSelector } from "@/hooks/useTypedSelector";
 import ArrowButton from "@/components/ui/button/Arrow";
 import { useTranslation } from "next-i18next";
-// TODO: to be uncommented when UserCard is ready
-// import UserCard from '@/components/cards/User';
-// TODO: to be uncommented when TranslationBox is ready
-// import TranslationBox from '~/components/cards/TranslationBox';
+import UserCard from "@/components/cards/User";
+import TranslationBox from "@/components/cards/TranslationBox";
+import { ReactElement } from "react";
 
+/**
+ * Type for the default locale
+ * @date 4/3/2023 - 12:38:50 PM
+ *
+ * @typedef {DefaultLocale}
+ */
+type DefaultLocale = "en" | "fr";
+
+/**
+ * Interface for SubmissionViewCard props
+ * @date 4/3/2023 - 12:36:51 PM
+ *
+ * @interface SubmissionViewCardProps
+ * @typedef {SubmissionViewCardProps}
+ */
 interface SubmissionViewCardProps {
-  submission?: {
-    user?: any;
-    created_at?: string;
-    text?: string;
-    link?: string;
-    metadata?: {
-      language?: string;
+  submission: {
+    user: any;
+    created_at: string;
+    text: string;
+    link: string;
+    metadata: {
+      language: string;
     };
   };
 }
 
-const SubmissionViewCard: React.FC<SubmissionViewCardProps> = ({
+/**
+ * Submission view card component
+ * @date 4/3/2023 - 12:38:14 PM
+ *
+ * @export
+ * @param {SubmissionViewCardProps} {
   submission = {},
-}) => {
+}
+ * @returns {ReactElement}
+ */
+export default function SubmissionViewCard({
+  submission,
+}: SubmissionViewCardProps): ReactElement {
   const { t } = useTranslation();
   const colors = useSelector((state) => state.ui.colors);
-  // TODO: to be uncommented when redux is ready
-  //   const community = useSelector((state: any) => state.communities.current);
-
-  const badgeButtonStyles = {
-    backgroundColor: colors.textAccent,
-    color: colors.text,
-  };
 
   const language = submission?.metadata?.language || "en";
-
   const primaryButtonStyles = {
     borderColor: colors.textAccent,
     color: colors.text,
@@ -42,36 +58,33 @@ const SubmissionViewCard: React.FC<SubmissionViewCardProps> = ({
     "--button-background-color--hover": colors.accent,
     "--button-border-color--hover": colors.accent,
   };
-  // TODO: to be uncommented when UserCard and TranslationBox are ready
-  // <UserCard
-  //   user={submission.user}
-  //   timestamp={{
-  //     date: submission.created_at,
-  //     text: t("submissions.submitted"),
-  //   }}
-  // >
-  //   <TranslationBox
-  //     className="pb-5"
-  //     text={submission.text}
-  //     defaultLocale={language}
-  //     textCssClasses="text-base md:text-lg leading-normal text-gray-700"
-  //   />
+
   return (
-    <div className="inline-grid space-y-2 md:space-y-5 md:contents space-x-0 md:space-x-2">
-      {submission.link && (
-        <ArrowButton
-          link={submission.link}
-          target="__blank"
-          customStyle={primaryButtonStyles}
-          variant="outline-primary"
-        >
-          {t("submissions.link.github")}
-        </ArrowButton>
-      )}
-    </div>
+    <UserCard
+      user={submission.user}
+      timestamp={{
+        date: submission.created_at,
+        text: t("submissions.submitted"),
+      }}
+    >
+      <TranslationBox
+        textContainerCssClasses="pb-5"
+        text={submission.text as string}
+        defaultLocale={language as DefaultLocale}
+        textCssClasses="text-base md:text-lg leading-normal text-gray-700"
+      />
+      <div className="inline-grid space-y-2 md:space-y-5 md:contents space-x-0 md:space-x-2">
+        {submission.link && (
+          <ArrowButton
+            link={submission.link}
+            target="__blank"
+            customStyle={primaryButtonStyles}
+            variant="outline-primary"
+          >
+            {t("submissions.link.github")}
+          </ArrowButton>
+        )}
+      </div>
+    </UserCard>
   );
-
-  //</UserCard>
-};
-
-export default SubmissionViewCard;
+}
