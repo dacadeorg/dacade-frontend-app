7e1ca7aef96f2a86061924dafedcdd9925aa6bae
serapieTuyishime <tuyiserapie1@gmail.com>
2023-04-03 13:16:34 +0200

    Feat: migrate balance component from list folder

---
+++ src/components/list/Balance.tsx
@@ -0,0 +1,57 @@
+import { useSelector } from "react-redux";
+import BalanceCard from "@/components/cards/Balance";
+import { useTranslation } from "react-i18next";
+import { ReactElement } from "react";
+
+/**
+ * Balance card props
+ * @date 4/3/2023 - 1:14:52 PM
+ *
+ * @interface BalanceListProps
+ * @typedef {BalanceListProps}
+ */
+interface BalanceListProps {
+  value?: number;
+}
+
+/**
+ * Function that return Balance card component
+ * @date 4/3/2023 - 1:15:06 PM
+ *
+ * @export
+ * @param {BalanceListProps} {
+  value = 0,
+}
+ * @returns {ReactElement}
+ */
+export default function BalanceList({
+  value = 0,
+}: BalanceListProps): ReactElement {
+  // TODO , to remove any when the stypes for state are available, i suppose after postman file arrives
+  const wallets: wallet[] = useSelector(
+    (state: any) => state.user.wallets.list
+  );
+
+  interface wallet {
+    token: string;
+    title: string;
+    address: string;
+    balance: number;
+    id?: number;
+  }
+
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-left">
+      <span className="uppercase block text-xs font-semibold text-gray-500 leading-relaxed">
+        {t("nav.balance")}
+      </span>
+      <div className="space-y-4 mt-2">
+        {wallets.map((wallet) => (
+          <BalanceCard key={wallet.id} details={wallet} />
+        ))}
+      </div>
+    </div>
+  );
+}
