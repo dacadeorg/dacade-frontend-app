3300aab8f47ec72feb33ce724ba8bc0322e83553
mbukerepo <mbuke404@gmail.com>
2023-04-03 20:59:32 +0200

    feat: implement communities slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import ui from "./feature/ui.slice";
+import { communitiesApi } from "./feature/communities.slice";
 
 export interface IRootState {
   ui: ReturnType<typeof ui.reducer>;
+  communities: ReturnType<typeof communitiesApi.reducer>;
 }
 
 const makeStore = () =>
   configureStore({
     reducer: {
       [ui.name]: ui.reducer,
+      [communitiesApi.reducerPath]: communitiesApi.reducer,
+    },
+    middleware(getDefaultMiddleware) {
+      return getDefaultMiddleware().concat(communitiesApi.middleware);
     },
     devTools: true,
   });
