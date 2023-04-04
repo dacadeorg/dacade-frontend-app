80451a904088943ac08b267f01fc62ff66a96a7c
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-04 17:49:16 +0200

    feat: add comment to the user slice

--- src/store/feature/user.slice.ts
+++ src/store/feature/user.slice.ts
@@ -3,6 +3,15 @@ import { auth as firebaseAuth } from "@/plugins/firebase";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { User } from "@/types/bounty";
 
+/**
+ * The default state for the user slice.
+ * @typedef {Object} DefaultState
+ * @property {User|null} data - The current user data or null if no user is logged in.
+ * @property {string|null} userBalance - The user's balance or null if the user is not logged in.
+ * @property {string|null} balance - The user's balance or null if the user is not logged in.
+ * @property {string|null} walletAddresses - The user's wallet addresses or null if the user is not logged in.
+ * @property {string|null} token - The user's token or null if the user is not logged in.
+ */
 interface DefaultState {
   data: User | null;
   userBalance: string | null;
@@ -11,6 +20,10 @@ interface DefaultState {
   token: string | null;
 }
 
+/**
+ * The default state for the user slice.
+ * @type {DefaultState}
+ */
 const defaultState: DefaultState = {
   data: null,
   userBalance: null,
@@ -19,6 +32,10 @@ const defaultState: DefaultState = {
   token: null,
 };
 
+/**
+ * An async action creator that clears all user state.
+ * @type {AsyncThunk<undefined, undefined, {}>}
+ */
 export const clearState = createAsyncThunk(
   "user/clear",
   async (_, { dispatch }) => {
@@ -29,6 +46,10 @@ export const clearState = createAsyncThunk(
   }
 );
 
+/**
+ * An async action creator that fetches the current user data.
+ * @type {AsyncThunk<User|null, undefined, {}>}
+ */
 export const fetchUser = createAsyncThunk(
   "user/fetch",
   async (_, { dispatch }) => {
@@ -49,6 +70,10 @@ export const fetchUser = createAsyncThunk(
   }
 );
 
+/**
+ * An async action creator that updates the user's data.
+ * @type {AsyncThunk<undefined, any, {}>}
+ */
 export const update = createAsyncThunk(
   "user/update",
   async (payload, { dispatch }) => {
@@ -57,12 +82,20 @@ export const update = createAsyncThunk(
   }
 );
 
+/**
+ * Function that gets the current user's token.
+ * @returns {Promise<string|null>} The current user's token or null if the user is not logged in.
+ */
 export const getUserToken = async () => {
   const user = firebaseAuth.currentUser;
   const token = await user?.getIdToken();
   return token;
 };
 
+/**
+ * An async action creator that gets the current user's token and sets it in the state.
+ * @type {AsyncThunk<string|null, undefined, {}>}
+ */
 export const getToken = createAsyncThunk(
   "user/getToken",
   async (_, { dispatch }) => {
