aea680b56aac16038bb70aedc65b7641e2b5f0f6
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-04-11 20:58:39 +0200

    feat: import reputationCard in reputationlist

--- src/components/list/Reputation.tsx
+++ src/components/list/Reputation.tsx
@@ -1,28 +1,27 @@
-import { ReactElement } from 'react'
-import { useTranslation } from 'react-i18next'
-
-// TODO: uncomment this import once reputation card is implemented and redux intergration
-// TODO: import { useSelector } from 'react-redux'
-// TODO: import ReputationCard from '@/components/cards/Reputation'
+import { ReactElement } from "react";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import ReputationCard from "@/components/cards/Reputation";
 
 export default function ReputationList(): ReactElement {
-//  TODO: const reputations = useSelector(state => state.user.reputations.list)
-  const { t } = useTranslation()
+  // TODO: To be uncommented when the use slice is implemented
+  // const reputations = useSelector(state => state.user.reputations.list)
+  const { t } = useTranslation();
 
   return (
     <div className="text-left">
       <span className="uppercase block text-xs font-semibold text-gray-500 leading-relaxed">
-        {t('nav.reputation')}
+        {t("nav.reputation")}
       </span>
       <div className="space-y-4 mt-2">
         {/* 
-           TODO: uncomment this ReputationCard rendering once the reputation is ready
-            {reputations.map(reputation => (
-                <ReputationCard key={reputation.id} details={reputation} />
-            ))} 
-
+          // TODO: To be uncommented when the use slice is implemented
+          {
+            reputations.map(reputation => (
+              <ReputationCard key={reputation.id} details={reputation} />
+            ))}  
         */}
       </div>
     </div>
-  )
+  );
 }
