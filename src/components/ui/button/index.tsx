1d7751e58f117c9ee18134f1b7cf4700460c987f
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-12 11:59:29 +0200

    fix: fix the build error

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -22,7 +22,7 @@ interface ButtonProps {
   loading?: boolean;
   disabled?: boolean;
   rounded?: boolean;
-  type: "submit" | "button" | "reset" | undefined;
+  type?: "submit" | "button" | "reset" | undefined;
   variant: string;
   padding?: boolean | string;
   margin?: string;
