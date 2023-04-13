881ed83543854c58b3ef1bf1ead19d9b3d4ded5c
mbukerepo <mbuke404@gmail.com>
2023-04-13 04:47:30 +0200

    feat: implement fetchCommunity given slug

--- src/services/community.ts
+++ src/services/community.ts
@@ -1,7 +1,15 @@
-import axiosInstance from '@/config/axios';
-import { Community } from '@/types/community';
+import api from "@/config/api";
+import axiosInstance from "@/config/axios";
+import { Community } from "@/types/community";
 
 export const fetchCommunities = async (): Promise<Community[]> => {
-  const response = await axiosInstance.get('communities');
+  const response = await axiosInstance.get("communities");
+  return response.data;
+};
+
+export const fetchCommunity = async (
+  slug: string
+): Promise<Community> => {
+  const response = await api().server.get(`communities/${slug}`);
   return response.data;
 };
