4a4799bf2399068c501574028ba8d88f59a7ef30
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-14 11:05:09 +0200

    feat: implement TODOs

--- src/pages/email-verification.tsx
+++ src/pages/email-verification.tsx
@@ -1,74 +1,80 @@
+/*
+ TODO: The logic for this is file need to be improved 
+ when the login and signup logic is fully tested.
+*/
 import ArrowButton from "@/components/ui/button/Arrow";
+import { useSelector } from "@/hooks/useTypedSelector";
+import { resendEmailVerification } from "@/store/feature/auth.slice";
 import i18Translate from "@/utilities/I18Translate";
 import { GetStaticProps } from "next";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-
-// TODO: user data will be uncommented when the redux store is ready
-import { useSelector } from "react-redux";
+import { ReactElement } from "react-markdown/lib/react-markdown";
 
 /**
  * Email verification page
  * @date 4/6/2023 - 7:11:31 PM
  *
  * @export
- * @returns {*}
+ * @returns {ReactElement}
  */
-export default function EmailVerification() {
+
+export default function EmailVerification(): ReactElement {
   const { t } = useTranslation();
+  const router = useRouter();
+  const [loading, setloading] = useState(false);
+  const user = useSelector((state) => state.auth.data);
 
-  // TODO: user data will be uncommented when the redux store is ready
-  //   const { user } = useSelector((state: any) => state?.auth);
-
-  // this will be removed when the redux store is ready
-  const user = { email: "johndoe@gmail.com" };
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [router, user]);
 
   const resendEmail = async () => {
-    //TODO: will be uncommented when the redux store is ready
-    // setloading(true);
-    // try {
-    //   const data = await dispatch("auth/resendEmailVerification");
-    // } catch (e) {
-    //   console.log(e);
-    // } finally {
-    //   setloading(false);
-    // }
+    setloading(true);
+    try {
+      await resendEmailVerification();
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setloading(false);
+    }
   };
 
-  const [loading, setloading] = useState(false);
+  if (user)
+    return (
+      <div className="flex items-center justify-center absolute min-h-screen top-0 w-full">
+        <div className="relative p-6 text-center">
+          <div>
+            <h1 className="text-3xl font-medium mb-7">
+              {t("email-verification.title")}
+            </h1>
+            <p className="text-lg">
+              {t("email-verification.subtitle")}
+            </p>
+            <p className="text-base font-bold mb-4">{user.email}</p>
+            <p
+              className="text-lg py-4"
+              dangerouslySetInnerHTML={{
+                __html: t("email-verification.message"),
+              }}
+            />
+            <p className="text-sm py-4">
+              {t("email-verification.foot-note")}
+            </p>
+          </div>
 
-  return user ? (
-    <div className="flex items-center justify-center absolute min-h-screen top-0 w-full">
-      <div className="relative p-6 text-center">
-        <div>
-          <h1 className="text-3xl font-medium mb-7">
-            {t("email-verification.title")}
-          </h1>
-          <p className="text-lg">
-            {t("email-verification.subtitle")}
-          </p>
-          <p className="text-base font-bold mb-4">{user.email}</p>
-          <p
-            className="text-lg py-4"
-            dangerouslySetInnerHTML={{
-              __html: t("email-verification.message"),
-            }}
-          />
-          <p className="text-sm py-4">
-            {t("email-verification.foot-note")}
-          </p>
-        </div>
-
-        <div className="text-center mt-1">
-          <ArrowButton loading={loading} onClick={resendEmail}>
-            {t("email-verification.buttons.resend")}
-          </ArrowButton>
+          <div className="text-center mt-1">
+            <ArrowButton loading={loading} onClick={resendEmail}>
+              {t("email-verification.buttons.resend")}
+            </ArrowButton>
+          </div>
         </div>
       </div>
-    </div>
-  ) : (
-    <>User Not available</>
-  );
+    );
+  return <></>;
 }
 
 export const getStaticProps: GetStaticProps = async ({ locale }) =>
