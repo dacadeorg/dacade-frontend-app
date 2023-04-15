d45753a948b63cd9f9f1fd2b45591da692eaf305
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-04-15 09:42:31 +0200

    chore: collect all of the epic issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,7 @@ const Home = (props: { pageProps: { communities: Community[] } }) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* TODO: The max with should be removed when the home page layout is migrated */}
-      <main className="relative max-w-7xl mx-auto">
+      <main>
         <MainSection />
         <CommunitiesSection communities={communities} />
         <TestimonialsSection />
