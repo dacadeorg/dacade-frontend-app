cc30ca68ebd8dab915d1207567b606a6a7d777fd
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-16 15:47:25 +0200

    fix: fix scoreboard card UI on mobile

--- src/components/cards/Scoreboard.tsx
+++ src/components/cards/Scoreboard.tsx
@@ -43,8 +43,8 @@ export default function ScoreboardCard({
   const { t } = useTranslation();
 
   return (
-    <div className="relative w-full flex sm:space-x-5 space-y-0 sm:flex-row-reverse sm:space-x-reverse sm:justify-between overflow-hidden bg-gray-50 sm:p-7 py-5 px-6 sm:items-center">
-      <div className="sm:flex-none absolute bottom-5 left-6 sm:relative sm:inset-0">
+    <div className="relative w-full md:flex sm:space-x-5 space-y-0 sm:flex-row-reverse sm:space-x-reverse sm:justify-between overflow-hidden bg-gray-50 sm:p-7 py-5 px-6 sm:items-center">
+      <div className="sm:flex-none absolute bottom-5 left-6 md:relative md:inset-0">
         <div className="font-medium text-gray-500 px-2.5 py-0.5 h-6 text-xxs bg-gray-200 rounded-full">
           <Currency value={value.score} token="REP" />
         </div>
@@ -54,7 +54,7 @@ export default function ScoreboardCard({
           {value.user.displayName}
         </div>
         <div className="flex flex-row divide-x divide-solid divide-gray-300">
-          <div className="whitespace-nowrap text-base pr-4">
+          <div className="whitespace-nowrap text-base sm:pr-4 pr-3">
             <span className="font-normal text-xs">
               {value.feedbacks}
               <span className="font-normal text-xs">
@@ -64,7 +64,7 @@ export default function ScoreboardCard({
               </span>
             </span>
           </div>
-          <div className="whitespace-nowrap text-base px-4">
+          <div className="whitespace-nowrap text-base sm:px-4 px-3">
             <span className="font-normal text-xs">
               {value.score}
               <span className="font-normal text-xs">
@@ -84,7 +84,7 @@ export default function ScoreboardCard({
           </div>
         </div>
       </div>
-      <div className="relative sm:flex-none pt-9 sm:p-0">
+      <div className="relative float-right md:float-left sm:flex-none pt-1 sm:p-0">
         <div className="relative sm:inset-0">
           <Avatar user={value?.user} size="large" shape="full" />
           <Badge
