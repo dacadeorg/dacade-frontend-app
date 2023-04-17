66b2917e2b2995fa86856736b79272f0fa1785e0
serapieTuyishime <tuyiserapie1@gmail.com>
2023-04-17 12:40:32 +0200

    feat: Navigate Content partial in sections/courses folder

---
+++ src/components/sections/courses/_partials/navigation/link/Content.tsx
@@ -0,0 +1,57 @@
+import ChevronRightIcon from "@/icons/chevron-right.svg";
+import { ReactElement } from "react";
+
+/**
+ * NavItem props
+ * @date 4/17/2023 - 12:39:33 PM
+ *
+ * @interface NavItemProps
+ * @typedef {NavItemProps}
+ */
+interface NavItemProps {
+  item: {
+    label: string;
+    subitems?: any[];
+  };
+  isActive?: boolean;
+  expanded?: boolean;
+}
+
+/**
+ * NavItem component
+ * @date 4/17/2023 - 12:39:14 PM
+ *
+ * @export
+ * @param {NavItemProps} {
+  item,
+  isActive = false,
+  expanded = false,
+}
+ * @returns {ReactElement}
+ */
+export default function NavItem({
+  item,
+  isActive = false,
+  expanded = false,
+}: NavItemProps): ReactElement {
+  return (
+    <span>
+      <span
+        style={{ display: isActive ? "inline-block" : "none" }}
+        className="absolute top-0 inline-block -left-6 nav-icon"
+      >
+        <ChevronRightIcon
+          className={`transition-transform duration-200 ${
+            item.subitems &&
+            item.subitems.length &&
+            isActive &&
+            expanded
+              ? "transform rotate-90"
+              : ""
+          }`}
+        />
+      </span>
+      <span className="nav-label">{item.label}</span>
+    </span>
+  );
+}
