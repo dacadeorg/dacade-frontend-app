ae11319ca47cf0cb23fe2bc0adf1e6fd29ff4b3c
serapieTuyishime <tuyiserapie1@gmail.com>
2023-04-19 11:08:00 +0200

    Refact: apply the getLayout as on login and signup pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -66,101 +66,101 @@ export default function Login(): ReactElement {
 
   return (
     <>
-      <LayoutWithoutFooter>
-        <Head>
-          <title>
-            {getMetadataTitle(t("login-page.signin.title"))}
-          </title>
-        </Head>
-        <div className="absolute h-full w-full -justify-center top-0 flex items-center">
-          <form
-            className="content-wrapper"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="lg:w-98 xl:w-98 mx-auto">
+      <Head>
+        <title>
+          {getMetadataTitle(t("login-page.signin.title"))}
+        </title>
+      </Head>
+      <div className="absolute h-full w-full -justify-center top-0 flex items-center">
+        <form
+          className="content-wrapper"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="lg:w-98 xl:w-98 mx-auto">
+            <div>
+              <h1 className="text-5xl my-5">
+                {t("login-page.signin.title")}
+              </h1>
+            </div>
+            <div className="mb-5 relative">
               <div>
-                <h1 className="text-5xl my-5">
-                  {t("login-page.signin.title")}
-                </h1>
-              </div>
-              <div className="mb-5 relative">
-                <div>
-                  <Input
-                    /* In backticks `` because placeholder requires a string.
-                     * Same for label
-                     */
-                    placeholder={`${t(
-                      "login-page.email.placeholder"
-                    )}`}
-                    label={`${t("login-page.email.label")}`}
-                    error={errors.email?.message}
-                    type="email"
-                    value={emailValue}
-                    {...register("email", {
-                      required: "This field is required",
-                      pattern: {
-                        value:
-                          /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i,
-                        message: "This must be a valid email address",
-                      },
-                    })}
-                  />
-                </div>
-              </div>
-              <div>
-                <div>
-                  <Input
-                    /* In backticks `` because placeholder requires a string.
-                     * Same for label
-                     */
-                    placeholder={`${t(
-                      "login-page.password.placeholder"
-                    )}`}
-                    label={`${t("login-page.password.label")}`}
-                    error={errors.password?.message}
-                    type="password"
-                    value={passwordValue}
-                    onInput={(value) => setPasswordValue(value)}
-                    {...register("password", {
-                      required: "This field is required",
-                      minLength: {
-                        value: 6,
-                        message: "The password is too short",
-                      },
-                    })}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col justify-between mt-4">
-                <div>
-                  <span className="text-primary text-sm">
-                    <Link
-                      href="/password-reset"
-                      className="fs-1 dark-white"
-                    >
-                      {t("login-page.forget-password")}
-                    </Link>
-                  </span>
-                </div>
-                <div className="text-right self-end">
-                  <ArrowButton
-                    type="submit"
-                    minWidthClass="min-w-40"
-                    arrowClasses="text-white"
-                    loading={loading}
-                    disabled={loading}
-                  >
-                    {t("login-page.signin.button")}
-                  </ArrowButton>
-                </div>
+                <Input
+                  /* In backticks `` because placeholder requires a string.
+                   * Same for label
+                   */
+                  placeholder={`${t("login-page.email.placeholder")}`}
+                  label={`${t("login-page.email.label")}`}
+                  error={errors.email?.message}
+                  type="email"
+                  value={emailValue}
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value:
+                        /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i,
+                      message: "This must be a valid email address",
+                    },
+                  })}
+                />
               </div>
             </div>
-          </form>
-        </div>
-      </LayoutWithoutFooter>
+            <div>
+              <div>
+                <Input
+                  /* In backticks `` because placeholder requires a string.
+                   * Same for label
+                   */
+                  placeholder={`${t(
+                    "login-page.password.placeholder"
+                  )}`}
+                  label={`${t("login-page.password.label")}`}
+                  error={errors.password?.message}
+                  type="password"
+                  value={passwordValue}
+                  onInput={(value) => setPasswordValue(value)}
+                  {...register("password", {
+                    required: "This field is required",
+                    minLength: {
+                      value: 6,
+                      message: "The password is too short",
+                    },
+                  })}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-between mt-4">
+              <div>
+                <span className="text-primary text-sm">
+                  <Link
+                    href="/password-reset"
+                    className="fs-1 dark-white"
+                  >
+                    {t("login-page.forget-password")}
+                  </Link>
+                </span>
+              </div>
+              <div className="text-right self-end">
+                <ArrowButton
+                  type="submit"
+                  minWidthClass="min-w-40"
+                  arrowClasses="text-white"
+                  loading={loading}
+                  disabled={loading}
+                >
+                  {t("login-page.signin.button")}
+                </ArrowButton>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
+
+Login.getLayout = function (page: ReactElement) {
+  return <LayoutWithoutFooter>{page}</LayoutWithoutFooter>;
+};
 
 export const getStaticProps: GetStaticProps = async ({ locale }) =>
   i18Translate(locale as string);
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -76,171 +76,165 @@ export default function Signup(): ReactElement {
 
   return (
     <>
-      <LayoutWithoutFooter>
-        <Head>
-          <title>
-            {getMetadataTitle(t("login-page.signup.title"))}
-          </title>
-        </Head>
-        <div className="absolute w-full top-0 min-h-screen flex items-center">
-          <form
-            className="content-wrapper pt-24"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="lg:w-98 xl:w-98 mx-auto">
-              {referrer && (
-                <div className="p-px">
-                  <h1 className="text-.5xl mb-2.5 font-medium leading-6 text-gray-900">
-                    <span className="capitalize">{referrer}</span>
-                    {t("signup-page.referrer.title")}
-                    {t("app.name")}
-                  </h1>
-                  <p
-                    className={classNames("my-px text-gray-700", {
-                      invisible: !(referrals && referrals.length),
-                    })}
-                  >
-                    {t("signup-page.referrer.subtitle")}
-                  </p>
-
-                  {referrals && referrals.length && (
-                    <div className="my-8">
-                      <ReferralsList bounty />
-                    </div>
-                  )}
-                </div>
-              )}
-
-              {!referrer && (
-                <h1 className="text-5xl my-5">
-                  {t("login-page.signup.title")}
+      <Head>
+        <title>
+          {getMetadataTitle(t("login-page.signup.title"))}
+        </title>
+      </Head>
+      <div className="absolute w-full top-0 min-h-screen flex items-center">
+        <form
+          className="content-wrapper pt-24"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="lg:w-98 xl:w-98 mx-auto">
+            {referrer && (
+              <div className="p-px">
+                <h1 className="text-.5xl mb-2.5 font-medium leading-6 text-gray-900">
+                  <span className="capitalize">{referrer}</span>
+                  {t("signup-page.referrer.title")}
+                  {t("app.name")}
                 </h1>
-              )}
-
-              <div className="mb-5 relative">
-                <div>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder={`${t(
-                      "login-page.email.placeholder"
-                    )}`}
-                    label={`${t("login-page.email.label")}`}
-                    error={errors.email?.message}
-                    {...register("email", {
-                      required: "This field is required",
-                      pattern: {
-                        value:
-                          /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i,
-                        message: "This must be a valid email address",
-                      },
-                    })}
-                  />
-                </div>
-              </div>
-              <div className="mb-5 relative">
-                <Input
-                  id="username"
-                  placeholder={`${t(
-                    "login-page.username.placeholde"
-                  )}`}
-                  label={`${t("login-page.username.label")}`}
-                  error={errors.username?.message}
-                  {...register("username", {
-                    required: "This field is required",
-                    minLength: {
-                      value: 3,
-                      message: "The username is too short",
-                    },
+                <p
+                  className={classNames("my-px text-gray-700", {
+                    invisible: !(referrals && referrals.length),
                   })}
-                />
-              </div>
-              <div className="mb-5 relative">
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder={`${t(
-                    "login-page.password.placeholde"
-                  )}`}
-                  label={`${t("login-page.password.label")}`}
-                  error={errors.password?.message}
-                  {...register("password", {
-                    required: "This field is required",
-                    minLength: {
-                      value: 3,
-                      message: "The password is too short",
-                    },
-                  })}
-                />
-              </div>
-              {!referrer && (
-                <div className="mb-5 relative">
-                  <Input
-                    id="referralCode"
-                    placeholder={`${t(
-                      "login-page.refcode.placeholder"
-                    )}`}
-                    label={`${t("login-page.refcode.label")}`}
-                    error={errors.referralCode?.message}
-                    {...register("referralCode", {
-                      minLength: {
-                        value: 3,
-                        message: "The referral code is too short",
-                      },
-                    })}
-                  />
-                </div>
-              )}
+                >
+                  {t("signup-page.referrer.subtitle")}
+                </p>
 
-              <div className="flex justify-between mt-4">
-                <div className="flex flex-col self-start">
-                  <div className="max-w-xm">
-                    <div className="flex space-x-3">
-                      <div>
-                        <Checkbox
-                          id="terms-checkbox"
-                          {...register("checkTerms", {
-                            required: `${t(
-                              "signup-page.terms.warning"
-                            )}`,
-                          })}
-                        />
-                      </div>
-                      <div className="max-w-none test">
-                        <p>I agree to {t("app.name")}&#8217;s</p>
-                        <Link
-                          className="underline"
-                          href="/terms-conditions"
-                        >
-                          {t("signup-page.terms")}
-                        </Link>
-                      </div>
-                    </div>
-                    <span className="text-red-600">
-                      {errors.checkTerms?.message}
-                    </span>
+                {referrals && referrals.length && (
+                  <div className="my-8">
+                    <ReferralsList bounty />
                   </div>
-                </div>
+                )}
+              </div>
+            )}
 
-                <div className="flex text-right self-start">
-                  <ArrowButton
-                    loading={loading}
-                    type="submit"
-                    minWidthClass="min-w-40"
-                    arrowClasses="text-white"
-                    disabled={loading}
-                  >
-                    {t("login-page.signup.button")}
-                  </ArrowButton>
-                </div>
+            {!referrer && (
+              <h1 className="text-5xl my-5">
+                {t("login-page.signup.title")}
+              </h1>
+            )}
+
+            <div className="mb-5 relative">
+              <div>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder={`${t("login-page.email.placeholder")}`}
+                  label={`${t("login-page.email.label")}`}
+                  error={errors.email?.message}
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value:
+                        /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i,
+                      message: "This must be a valid email address",
+                    },
+                  })}
+                />
               </div>
             </div>
-          </form>
-        </div>
-      </LayoutWithoutFooter>
+            <div className="mb-5 relative">
+              <Input
+                id="username"
+                placeholder={`${t("login-page.username.placeholde")}`}
+                label={`${t("login-page.username.label")}`}
+                error={errors.username?.message}
+                {...register("username", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 3,
+                    message: "The username is too short",
+                  },
+                })}
+              />
+            </div>
+            <div className="mb-5 relative">
+              <Input
+                id="password"
+                type="password"
+                placeholder={`${t("login-page.password.placeholde")}`}
+                label={`${t("login-page.password.label")}`}
+                error={errors.password?.message}
+                {...register("password", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 3,
+                    message: "The password is too short",
+                  },
+                })}
+              />
+            </div>
+            {!referrer && (
+              <div className="mb-5 relative">
+                <Input
+                  id="referralCode"
+                  placeholder={`${t(
+                    "login-page.refcode.placeholder"
+                  )}`}
+                  label={`${t("login-page.refcode.label")}`}
+                  error={errors.referralCode?.message}
+                  {...register("referralCode", {
+                    minLength: {
+                      value: 3,
+                      message: "The referral code is too short",
+                    },
+                  })}
+                />
+              </div>
+            )}
+
+            <div className="flex justify-between mt-4">
+              <div className="flex flex-col self-start">
+                <div className="max-w-xm">
+                  <div className="flex space-x-3">
+                    <div>
+                      <Checkbox
+                        id="terms-checkbox"
+                        {...register("checkTerms", {
+                          required: `${t(
+                            "signup-page.terms.warning"
+                          )}`,
+                        })}
+                      />
+                    </div>
+                    <div className="max-w-none test">
+                      <p>I agree to {t("app.name")}&#8217;s</p>
+                      <Link
+                        className="underline"
+                        href="/terms-conditions"
+                      >
+                        {t("signup-page.terms")}
+                      </Link>
+                    </div>
+                  </div>
+                  <span className="text-red-600">
+                    {errors.checkTerms?.message}
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex text-right self-start">
+                <ArrowButton
+                  loading={loading}
+                  type="submit"
+                  minWidthClass="min-w-40"
+                  arrowClasses="text-white"
+                  disabled={loading}
+                >
+                  {t("login-page.signup.button")}
+                </ArrowButton>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
-
+Signup.getLayout = function (page: ReactElement) {
+  return <LayoutWithoutFooter>{page}</LayoutWithoutFooter>;
+};
 export const getStaticProps: GetStaticProps = async ({ locale }) =>
   i18Translate(locale as string);
