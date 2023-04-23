455945b5cea9b9804505a55664b59f6ee84be6d7
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-23 12:02:11 +0200

    feat: apply all the TODOs

--- src/components/sections/learning-modules/Material.tsx
+++ src/components/sections/learning-modules/Material.tsx
@@ -1,30 +1,12 @@
 import Slugger from "github-slugger";
-//  TODO this line will be uncommented once learning module Markdown component has been merged and fragment could be removed during.
-// import Markdown from './_partials/Markdown';
 import Section from "@/components/sections/communities/_partials/Section";
-//  TODO this line will be uncommented once Duration component
-// import Duration from '@/components/sections/courses/_partials/Duration';
+import Duration from "@/components/sections/courses/_partials/Duration";
 import ArrowButton from "@/components/ui/button/Arrow";
 import Video from "@/components/ui/Video";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
-
-/**
- * Material interface
- * @date 4/20/2023 - 10:36:35 AM
- *
- * @interface Material
- * @typedef {Material}
- */
-interface Material {
-  type: string;
-  title: string;
-  subtitle?: string;
-  duration?: number;
-  link: string;
-  list: { link: string }[];
-  description?: string;
-}
+import Markdown from "./_partials/MarkDown";
+import { Material, MaterialType } from "@/types/course";
 
 /**
  * Material component
@@ -37,13 +19,13 @@ interface MaterialProps {
   material?: Material;
 }
 
-export function Material({ material }: MaterialProps) {
+export default function MaterialSection({ material }: MaterialProps) {
   const sluggify = (text: string) => {
     const slugger = new Slugger();
     return slugger.slug(text);
   };
 
-  const isAdditional = material?.type === "ADDITIONAL";
+  const isAdditional = material?.type === MaterialType.ADDITIONAL;
   const materialId = !(!material?.title || isAdditional)
     ? sluggify(material?.title)
     : "";
@@ -55,25 +37,21 @@ export function Material({ material }: MaterialProps) {
       title={isAdditional ? "Additional Material" : material?.title}
     >
       {!isAdditional && (
-        //  TODO this line will be uncommented once Duration component has been merged and fragment could be removed during.
-        // <Duration
-        //   text={material?.subtitle}
-        //   value={material?.duration}
-        // />
-        <></>
+        <Duration
+          text={material?.subtitle!}
+          value={material?.duration!}
+        />
       )}
       {!isAdditional && (
         <span className="block text-lg mb-6 mt-2 md:w-99">
           {material?.description}
         </span>
       )}
-      {material?.type === "EMBEDDED-VIDEO" && (
+      {material?.type === MaterialType["EMBEDDED-VIDEO"] && (
         <Video url={material.link} />
       )}
-      {material?.type === "MARKDOWN" && (
-        // TODO this line will be uncommented once markdown component has been merged and fragment could be removed during.
-        // <Markdown url={material?.link} />
-        <></>
+      {material?.type === MaterialType.MARKDOWN && (
+        <Markdown url={material?.link} />
       )}
       {isAdditional && (
         <div>
@@ -91,14 +69,14 @@ export function Material({ material }: MaterialProps) {
                 {material?.title}
               </p>
               <span className="ml-2 leading-normal">
-                {/* TODO this line will be uncommented once Duration component  */}
-                {/* <Duration value={material?.duration} /> */}
+                <Duration value={material?.duration!} text={""} />
               </span>
             </Link>
           ))}
         </div>
       )}
-      {(material?.type === "TEXT" || material?.type === "ARTICLE") &&
+      {(material?.type === MaterialType.TEXT ||
+        material?.type === MaterialType.ARTICLE) &&
         material?.link && (
           <ArrowButton
             link={material?.link}
@@ -107,7 +85,7 @@ export function Material({ material }: MaterialProps) {
             community-styles={true}
             variant="outline-primary"
           >
-            {material?.type === "TEXT"
+            {material?.type === MaterialType.TEXT
               ? t("learning-module.material?.open.lesson")
               : t("learning-module.material?.open.article")}
           </ArrowButton>
