3f65011e640528d3305fa1242b7ad7aada84e2c5
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-23 11:40:02 +0200

    refactor: update the comment and refactor the conditional rendering set to the component

--- src/components/sections/courses/overview/index.tsx
+++ src/components/sections/courses/overview/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import RewardsSection from "./Rewards";
 import ObjectivesSection from "./Objectives";
 import PrerequisiteSection from "./Prerequisite";
@@ -15,22 +15,27 @@ import { useSelector } from "@/hooks/useTypedSelector";
  * @date 4/18/2023 - 12:24:38 PM
  *
  * @export
- * @returns {*}
+ * @returns {ReactElement}
  */
-export default function Overview() {
+export default function Overview(): ReactElement {
   const course = useSelector((state) => state.courses.current);
 
-  return course ? (
-    <div className="flex flex-col divide-y divide-solid divide-gray-200 lg:py-5 space-y-8 text-gray-700">
-      <Header title={course.name} description={course.description} />
-      <RewardsSection />
-      <ObjectivesSection />
-      <PrerequisiteSection />
-      <DisclaimerSection />
-      <TrailerSection />
-      <LearningModulesSection />
-      <ChallengeSection />
-      <PageNavigation />
-    </div>
-  ) : null;
+  if (course)
+    return (
+      <div className="flex flex-col divide-y divide-solid divide-gray-200 lg:py-5 space-y-8 text-gray-700">
+        <Header
+          title={course.name}
+          description={course.description}
+        />
+        <RewardsSection />
+        <ObjectivesSection />
+        <PrerequisiteSection />
+        <DisclaimerSection />
+        <TrailerSection />
+        <LearningModulesSection />
+        <ChallengeSection />
+        <PageNavigation />
+      </div>
+    );
+  return <></>;
 }
