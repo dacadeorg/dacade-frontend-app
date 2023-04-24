2c0d8a43568a1b4d4590451a0dfcb66defc972fe
Jonath-z <jonathanzihindula95@gmail.com>
2023-04-24 18:21:05 +0200

    fix: remove the explicit import of react

--- src/components/sections/courses/overview/index.tsx
+++ src/components/sections/courses/overview/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import RewardsSection from "./Rewards";
 import ObjectivesSection from "./Objectives";
 import PrerequisiteSection from "./Prerequisite";
