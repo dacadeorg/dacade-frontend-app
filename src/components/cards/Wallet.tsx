73350be05a30a87fb0287d73dd323b7ccea8d784
Samiskina <mureraksamantha@gmail.com>
2023-05-16 12:45:25 +0200

    feat: remove the react import

--- src/components/cards/Wallet.tsx
+++ src/components/cards/Wallet.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import { ReactElement, useState } from "react";
 import Coin from "@/components/ui/Coin";
 import ArrowButton from "@/components/ui/button/Arrow";
 import Tag from "@/components/ui/Tag";
