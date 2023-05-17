b5fd233ae84256784bcf1939dec39f04ce439eb5
serapieTuyishime <tuyiserapie1@gmail.com>
2023-05-17 17:51:53 +0200

    refact: remove Refferal type instead of using Referral

--- src/components/list/Bounty.tsx
+++ src/components/list/Bounty.tsx
@@ -1,8 +1,8 @@
 import Referral from "@/components/cards/Referral";
 import Bounty from "@/components/cards/Bounty";
 import { Bounty as BountyType } from "@/types/bounty";
-import { Refferral } from "@/types/referral";
 import { ReactElement, useEffect } from "react";
+import { Referral as ReferralType } from "@/types/community";
 
 /**
  * BountyList component props
@@ -13,7 +13,7 @@ import { ReactElement, useEffect } from "react";
  */
 interface BountyListProps {
   bounties: BountyType[];
-  referrals: Refferral[];
+  referrals: ReferralType[];
 }
 
 /**
@@ -31,7 +31,7 @@ export default function BountyList({ bounties = [], referrals = [] }: BountyList
   return (
     <div>
       {bounties && (
-        <div className="divide-y bg-gray-50 rounded-3xl space-y-0 divide-gray-200 px-0 mb-10 relative w-full lg:max-w-2xl overflow-hidden">
+        <div className="relative w-full px-0 mb-10 space-y-0 overflow-hidden divide-y divide-gray-200 bg-gray-50 rounded-3xl lg:max-w-2xl">
           {referrals.map((referral) => (
             <Referral referral={referral} key={referral.name} />
           ))}
