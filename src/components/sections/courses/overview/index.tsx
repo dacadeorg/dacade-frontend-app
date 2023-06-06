89534e10fedf8f595c836d6495aa4962f5135da8
Jonath-z <jonathanzihindula95@gmail.com>
2023-06-07 00:32:05 +0200

    Revert "fix: remove the divider between the disclaimer and course prerequisite"
    This reverts commit 6281fa2a4e086d8a53a1f8fbc32b88ab67a1966c.

--- src/components/sections/courses/overview/index.tsx
+++ src/components/sections/courses/overview/index.tsx
@@ -20,18 +20,19 @@ import { useSelector } from "@/hooks/useTypedSelector";
 export default function Overview(): ReactElement {
   const course = useSelector((state) => state.courses.current);
 
-  if (!course) return <></>;
-  return (
-    <div className="flex flex-col divide-y divide-solid divide-gray-200 lg:py-5 text-gray-700">
-      <Header title={course.name} description={course.description} />
-      <RewardsSection />
-      <ObjectivesSection />
-      <PrerequisiteSection />
-      <DisclaimerSection />
-      <TrailerSection />
-      <LearningModulesSection />
-      <ChallengeSection />
-      <PageNavigation />
-    </div>
-  );
+  if (course)
+    return (
+      <div className="flex flex-col divide-y divide-solid divide-gray-200 lg:py-5 space-y-8 text-gray-700">
+        <Header title={course.name} description={course.description} />
+        <RewardsSection />
+        <ObjectivesSection />
+        <PrerequisiteSection />
+        <DisclaimerSection />
+        <TrailerSection />
+        <LearningModulesSection />
+        <ChallengeSection />
+        <PageNavigation />
+      </div>
+    );
+  return <></>;
 }
