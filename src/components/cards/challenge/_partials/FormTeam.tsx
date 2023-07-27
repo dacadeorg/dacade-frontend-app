485b2d91e84fe58778bec0940267c5edf7684c64
mhenriette <mhenriette00@gmail.com>
2023-07-27 17:13:52 +0200

    fix: make formTeam responsive

--- src/components/cards/challenge/_partials/FormTeam.tsx
+++ src/components/cards/challenge/_partials/FormTeam.tsx
@@ -21,24 +21,20 @@ interface FormTeamCardProps {
 export default function FormTeamCard ({ index = 1, title, description }: FormTeamCardProps): JSX.Element {
     const path = `/communities/`; // This is link is not the actual link; we will replace it after it's done in the backend
   return (
-    <div className="flex flex-col w-2/5 relative p-6 divide-y sm:divide-y-0 sm:divide-x divide-gray-200 rounded-3xl group text-gray-700 sm:p-7 mb-4 border-solid border border-gray-200">
+    <div className="flex flex-col md:w-2/5 relative p-6 divide-y sm:divide-y-0 sm:divide-x divide-gray-200 rounded-3xl group text-gray-700 sm:p-7 mb-4 border-solid border border-gray-200">
       <div className="flex flex-col justify-between w-full sm:pb-0">
         <div className="flex flex-col">
           <div className="text-lg font-medium leading-normal text-gray-900">
-           <span>{index}.</span> <span className='ml-2'>{title} </span>
+            <span>{index}.</span> <span className="ml-2">{title} </span>
           </div>
-          <div className="text-sm font-normal text-gray-700 mt-3 max-w-xxs pb-6">
-            {description} 
-          </div>
+          <div className="text-sm font-normal text-gray-700 mt-3 max-w-xxs pb-6">{description}</div>
         </div>
-        <div className="">
-          <Link href={path}>
-            <ArrowButton communityStyles={true} variant="outline-primary">
-              Start now
-            </ArrowButton>
-          </Link>
-        </div>
+        <Link href={path}>
+          <ArrowButton communityStyles={true} variant="outline-primary">
+            Start now
+          </ArrowButton>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
