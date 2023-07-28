2184b9fa2b5ec4cbe3c9f3835ca9a504a46ee7cf
mhenriette <mhenriette00@gmail.com>
2023-07-28 17:53:52 +0200

    chore: add JSDOC comment

--- src/components/sections/challenges/ChallengeObjectives.tsx
+++ src/components/sections/challenges/ChallengeObjectives.tsx
@@ -1,13 +1,21 @@
 import ObjectiveList from "@/components/list/Objectives";
 import Section from "@/components/sections/communities/_partials/Section";
 import { useSelector } from "@/hooks/useTypedSelector";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
+import { ReactElement } from "react";
 
-export default function ChallengeObjectives() {
+/**
+ * Challenge objectives component
+ * @date 7/28/2023 - 5:04:54 PM
+ *
+ * @export
+ * @returns {ReactElement}
+ */
+export default function ChallengeObjectives(): ReactElement {
   const { t } = useTranslation();
   const challenge = useSelector((state) => state.challenges.current);
   return (
-    <Section title={t("communities.overview.challenge.objective.title") as string}>
+    <Section title={`${t("communities.overview.challenge.objective.title")}`}>
       <ObjectiveList objectives={challenge?.objectives} />
     </Section>
   );
