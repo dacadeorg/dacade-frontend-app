5467d8b2087cb0e0ebfdef5c1f6405bad904844b
mhenriette <mhenriette00@gmail.com>
2023-07-28 15:51:02 +0200

    feat: add challenge objective translation

--- src/components/sections/challenges/ChallengeObjectives.tsx
+++ src/components/sections/challenges/ChallengeObjectives.tsx
@@ -1,11 +1,13 @@
 import ObjectiveList from "@/components/list/Objectives";
 import Section from "@/components/sections/communities/_partials/Section";
 import { useSelector } from "@/hooks/useTypedSelector";
+import { useTranslation } from "react-i18next";
 
 export default function ChallengeObjectives() {
+  const { t } = useTranslation();
   const challenge = useSelector((state) => state.challenges.current);
   return (
-    <Section title="Challenge Objectives">
+    <Section title={t("communities.overview.challenge.objective.title") as string}>
       <ObjectiveList objectives={challenge?.objectives} />
     </Section>
   );
