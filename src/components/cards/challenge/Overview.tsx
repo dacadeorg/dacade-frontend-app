5499753b9f16c965369debdbd28958d49cc7b7ec
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2023-08-18 08:45:27 +0200

    fix: the rewards card for learning modules

--- src/components/cards/challenge/Overview.tsx
+++ src/components/cards/challenge/Overview.tsx
@@ -1,6 +1,7 @@
 import Coin from "@/components/ui/Coin";
 import { Challenge, Reward } from "@/types/course";
 import { useTranslation } from "next-i18next";
+import DateManager from "@/utilities/DateManager";
 
 /**
  * Renders a container with border, rounded corners, and spacing,
@@ -10,6 +11,7 @@ import { useTranslation } from "next-i18next";
  */
 export default function Overview({ challenge }: { challenge: Challenge }) {
   const { t } = useTranslation();
+  const expirationDate = challenge?.expiresAt && DateManager.format(challenge.expiresAt, "MMMM d, yyyy", "en");
   return (
     <div className="border mt-8 border-gray-200 rounded-3xl mb-5 group text-gray-700 p-6">
       <div className="text-gray-900 text-lg leading-normal">
@@ -19,26 +21,24 @@ export default function Overview({ challenge }: { challenge: Challenge }) {
       <div className="md:flex md:flex-row flex-col rounded-full max-w-max text-sm mt-6 space-y-8 md:space-x-8 md:space-y-0">
         {challenge.rewards.map((reward, index) => (
           <div key={`reward=${index}`} className="flex items-center">
-            <Coin size="medium" token={reward.token} />
+            <Coin size="medium" token={reward?.token} />
             <div className="text-sm md:pl-2 max-w-max">
-              <div className="flex text-gray-700 font-medium">
-                <span className="pr-1">{reward.amount}</span>
-                <span>
-                  {t("communities.overview.challenge.rewards", {
-                    count: parseInt(reward.token),
-                  })}
-                </span>
+              <div className="flex gap-1 text-gray-700 font-medium">
+                <span>{reward.amount}</span>
+                <span>{reward?.token}</span>
+                <span>{t("communities.overview.challenge.rewards")}</span>
               </div>
               <div className="text-gray-400 text-xs font-normal">{t("communities.overview.challenge.subtitle")}</div>
             </div>
           </div>
         ))}
       </div>
-      <div className="text-gray-400 text-sm font-normal pt-6">
-        <span>{t("communities.overview.challenge.deadline")}: </span>
-        {/* TODO: will be implemented when we have a challenge with expiration date or deadline. */}
-        <span className="font-medium">March 12th, 2022</span>
-      </div>
+      {expirationDate && (
+        <div className="text-gray-400 text-sm font-normal pt-6">
+          <span>{t("communities.overview.challenge.deadline")}: </span>
+          <span className="font-medium">{expirationDate}</span>
+        </div>
+      )}
     </div>
   );
 }
