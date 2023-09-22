0bc31b296f7fcbcd3a18500bfe3544fc8f59d790
Jonath-z <jonathanzihindula95@gmail.com>
2023-09-22 14:59:53 +0200

    refactor: remove unused codes

--- src/components/popups/DiscordDisconnect.tsx
+++ src/components/popups/DiscordDisconnect.tsx
@@ -1,12 +1,11 @@
-import { useState, useEffect, useCallback, ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Modal from "@/components/ui/Modal";
-import Loader from "@/components/ui/Loader";
 import Button from "@/components/ui/button";
 import { useTranslation } from "next-i18next";
-import { useRouter } from "next/router";
-// import api from "@/config/api";
 import { disconnectDiscord } from "@/store/feature/auth.slice";
 import { useDispatch } from "@/hooks/useTypedDispatch";
+import Loader from "@/components/ui/Loader";
+import { useRouter } from "next/router";
 
 /**
  * Discord Disconnect Props
@@ -16,101 +15,71 @@ import { useDispatch } from "@/hooks/useTypedDispatch";
  * @typedef {DiscordDisconnectProps}
  */
 interface DiscordDisconnectProps {
-    show: boolean;
-    username?: string;
-    onClose: () => void;
-  }
+  show: boolean;
+  username?: string;
+  onClose: () => void;
+}
 
-export default function DiscordDisconnect({ username, show, onClose }: DiscordDisconnectProps): ReactElement {
-    const [discordError, setDiscordError] = useState<string | boolean>(false);
-    const [discordSuccess, setDiscordSuccess] = useState(false);
-    const [discordLoading, setDiscordLoading] = useState(false);
-    const [showDiscordModal, setShowDiscordModal] = useState(false);
-    const { t } = useTranslation();
-    const dispatch = useDispatch();
-    const router = useRouter();
-    // const dispatch = useDispatch();
-  
-    const closeModal = () => {
-      setShowDiscordModal(false);
-      setDiscordError(false);
-      setDiscordSuccess(false);
-      setDiscordLoading(false);
-    };
-  
-    /**
-     * Get transilated discord status messages related to discord connection
-     * @date 4/4/2023 - 10:05:46 AM
-     *
-     * @returns {string}
-     */
-    // const getDiscordMessage = (): string => {
-    //   if (discordError) return t("profile.header.discord.error");
-    //   if (discordSuccess) return t("profile.header.discord.success");
-    //   return t("profile.header.discord.connect");
-    // };
-  
-    /**
-     * Connect or disconnect user from discord
-     * @date 4/4/2023 - 9:39:06 AM
-     *
-     * @type {Function}
-     */
-    // const discordCallback = useCallback(async () => {
-    //   try {
-    //     const { code } = router.query;
-    //     if (!code) {
-    //       return;
-    //     }
-    //     setDiscordLoading(true);
-    //     setShowDiscordModal(true);
-  
-    //     const response = await api().client.post("auth/discord/disconnect", {
-    //       code,
-    //     });
-  
-    //     if (!response) {
-    //       setDiscordError(true);
-    //       return;
-    //     }
-    //     setDiscordSuccess(true);
-    //     router.replace({ query: username ? { username: username } : {} });
-    //   } catch (error) {
-    //     setDiscordError(true);
-    //   } finally {
-    //     setDiscordLoading(false);
-    //   }
-    // }, []);
-  
-    // useEffect(() => {
-    //   discordCallback();
-    // }, [discordCallback]);
+export default function DiscordDisconnect({ show, onClose }: DiscordDisconnectProps): ReactElement {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [discordDisconnectError, setDiscordDisconnectError] = useState<string | boolean>(false);
+  const [discordDisconnectSuccess, setDiscordDisconnectSuccess] = useState(false);
+  const [disconnectLoading, setDisconnectLoading] = useState(false);
 
+  const getDiscordMessage = () => {
+    if (discordDisconnectError) return t("profile.header.discord.disconnect.error");
+    if (discordDisconnectSuccess) return t("profile.header.discord.disconnect.success");
+  };
 
-    const onDisconnect = () => {
-        dispatch(disconnectDiscord());
-      };
-  
-    return (
-      <Modal show={show} size="medium" onClose={onClose}>
-        <div className="px-6 pt-6">
-          <div className="pb-7">
-            <p className="text-.5xl font-medium leading-snug">{t("profile.header.discord.disconnect")}</p>
-          </div>
-  
-       <div className="flex flex-col space-x-3 pb-2">
+  const onDisconnect = async () => {
+    try {
+      setDisconnectLoading(true);
+      await dispatch(disconnectDiscord());
+      setDiscordDisconnectSuccess(true);
+      router.reload();
+    } catch (e) {
+      console.error(e);
+      setDiscordDisconnectError(true);
+    } finally {
+      setDisconnectLoading(false);
+    }
+  };
+
+  return (
+    <Modal show={show} size="medium" onClose={onClose}>
+      <div className="px-6 pt-6">
+        <div className="pb-7">
+          <p className="text-.5xl font-medium leading-snug">{t("profile.header.discord.disconnect")}</p>
+        </div>
+
+        {!disconnectLoading && !discordDisconnectSuccess && (
+          <div className="flex flex-col space-x-3 pb-2">
             <p> {t("profile.settings.discord.disconnect.confirm")}</p>
             <Button className="font-semibold w-1/4 mt-8 my-4 justify-self-center" onClick={onDisconnect} variant="outline-primary" type="button">
-            {t("profile.settings.discord.disconnect")}
+              {t("profile.settings.discord.disconnect")}
             </Button>
           </div>
+        )}
 
-          <div className="pb-7">
-            <Button disabled={discordLoading} className="-ml-4 font-semibold border-none!" variant="outline-primary" onClick={onClose} type="button">
-              {t("profile.header.discord.close")}
-            </Button>
-          </div>
+        <div className="flex space-x-3 pb-7">
+          {disconnectLoading && <Loader className="w-6 h-6 pt-6 text-green-400" />}
+          <p
+            className={`p-3 rounded text-.2xl font-medium leading-snug flex-1 ${
+              discordDisconnectError ? "bg-red-50 text-red-700" : discordDisconnectSuccess ? "bg-green-50 text-green-700" : "bg-white"
+            }`}
+          >
+            {getDiscordMessage()}
+          </p>
         </div>
-      </Modal>
-    );
-  }
+
+        <div className="pb-7">
+          <Button disabled={disconnectLoading} className="-ml-4 font-semibold border-none!" variant="outline-primary" onClick={onClose} type="button">
+            {t("profile.header.discord.close")}
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
