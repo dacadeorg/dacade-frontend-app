7b0f62e233a2e507c849b4f09364fa52ebdb59e7
mhenriette <mhenriette00@gmail.com>
2023-09-27 15:11:55 +0200

    fix: separate submissions and remove markdown

--- src/components/sections/submissions/List.tsx
+++ src/components/sections/submissions/List.tsx
@@ -52,7 +52,7 @@ export default function List(): ReactElement {
     <>
       {submissions && submissions.length ? (
         <div className="text-xl md:text-.5xl px-0 py-5 md:py-10 md:pb-5">
-          <div className="sm:border sm:border-gray-200 sm:border-solid rounded-3.5xl relative">
+          <div className="sm:border sm:border-gray-200 sm:border-solid rounded-3.5xl relative overflow-hidden">
             <div className="flex flex-col divide-y" id="scrollableDiv">
               <InfiniteScroll
                 dataLength={submissions.length}
@@ -61,9 +61,11 @@ export default function List(): ReactElement {
                 // loader is required for InfiniteScroll to work
                 loader={<></>}
               >
-                {submissions.map((submission, i) => (
-                  <SubmissionCard key={`submission-${i}`} submission={submission} />
-                ))}
+                <div className="divide-y divide-gray-200">
+                  {submissions.map((submission, i) => (
+                    <SubmissionCard key={`submission-${i}`} submission={submission} />
+                  ))}
+                </div>
               </InfiniteScroll>
             </div>
             {loading && <Loader loading={loading} className="sm:absolute sm:left-6 sm:-bottom-7.5" onClick={() => nextPage()} />}
