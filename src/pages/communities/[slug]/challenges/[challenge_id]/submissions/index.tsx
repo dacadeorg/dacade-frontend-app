e3a26ab0a7d23b78d626ee21bb392e79e14b35cc
serapieTuyishime <tuyiserapie1@gmail.com>
2023-10-05 16:48:06 +0200

    refact: remove the esLint ignore lines

--- src/pages/communities/[slug]/challenges/[challenge_id]/submissions/index.tsx
+++ src/pages/communities/[slug]/challenges/[challenge_id]/submissions/index.tsx
@@ -32,7 +32,7 @@ import { ReactElement, useCallback, useEffect, useMemo } from "react";
  * @returns
  */
 export default function Submission(props: { pageProps: { currentCommunity: Community; submissions: SubmissionType[]; challenge: Challenge } }) {
-  const { currentCommunity, submissions, challenge } = props.pageProps;
+  const { submissions, challenge } = props.pageProps;
   const selectedSubmission = useSelector((state) => state.submissions.current);
 
   const dispatch = useDispatch();
@@ -49,13 +49,13 @@ export default function Submission(props: { pageProps: { currentCommunity: Commu
 
   useEffect(() => {
     initChallengeNavigationMenu(navigation.community)(dispatch);
-    // Eslint desabled here for avoiding infinite rendering
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentCommunity, dispatch, submissions]);
+  }, [navigation.community, dispatch]);
 
+  // Temporary fix for links copied whose have submission_id as a query parameter
   useEffect(() => {
     if (submission_id) router.push(`${router.asPath.split("?")[0]}/${submission_id}`);
   }, [router, submission_id]);
+
   const headerPaths = useMemo(() => [t("communities.navigation.challenge")], [t]);
 
   if (!submissions) return <></>;
