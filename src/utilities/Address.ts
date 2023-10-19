f881dfabc69f6fca07a5d61ab948c71c653fb252
serapieTuyishime <tuyiserapie1@gmail.com>
2023-10-19 18:19:25 +0200

    feat: include icp wallet address valiadations

--- src/utilities/Address.ts
+++ src/utilities/Address.ts
@@ -1,7 +1,9 @@
+const aeAllowedChars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 const ethRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
-const aeRegex = /^(ak_)([${aeAllowedChars}]+)$/;
+const aeRegex = new RegExp(`^(ak_)[${aeAllowedChars}]+$`);
 const nearRegex = /^([a-fA-F0-9]{5})+([a-fA-F0-9]{49})+([a-fA-F0-9]{10})$/;
 const algoRegex = /^([A-Z2-7]{6})+([A-Z2-7]{46})+([A-Z2-7]{6})$/;
+const icpRegex = /`^[a-fA-F0-9]{64}$`/;
 
 /**
  * Validates the regex pattern
@@ -122,8 +124,12 @@ export const validateAddress = (address?: string, token: string = "eth") => {
     return validateRegex(trimmedAddress, aeRegex);
   }
 
-  if (tokenLowerCase === "algo") {
+  if (["algo", "usdc"].includes(tokenLowerCase)) {
     return validateRegex(trimmedAddress, algoRegex);
+  }
+
+  if (tokenLowerCase === "icp") {
+    return validateRegex(trimmedAddress, icpRegex);
   }
 
   return validateRegex(trimmedAddress, ethRegex);
