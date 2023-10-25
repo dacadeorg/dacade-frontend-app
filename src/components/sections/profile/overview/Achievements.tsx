39400bce2f48a1bb43c0209946cb125a3daef312
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2023-10-25 14:25:09 +0200

    fix: the expand of the achievement cards

--- src/components/sections/profile/overview/Achievements.tsx
+++ src/components/sections/profile/overview/Achievements.tsx
@@ -1,9 +1,9 @@
+
+import React, { ReactElement, useState } from "react";
 import ProfileOverviewSection from "@/components/sections/profile/overview/Section";
 import AchievementCard from "@/components/cards/Achievement";
 import { useSelector } from "@/hooks/useTypedSelector";
-import { ReactElement } from "react";
 import Plus from "@/icons/plus.svg";
-import Link from "next/link";
 
 /**
  * profile- Achievents component
@@ -14,22 +14,25 @@ import Link from "next/link";
  */
 export default function ProfileOverviewAchievements(): ReactElement {
   const achievements = useSelector((state) => state.profileCertificate.list);
+  const [showAll, setShowAll] = useState(false);
 
-  if (achievements && achievements.length)
+  if (achievements && achievements.length) {
+    const displayedAchievements = showAll ? achievements : achievements.slice(0, 4);
+
     return (
       <ProfileOverviewSection title="Achievements">
         <div className="grid grid-cols-2 gap-5 md:grid-cols-3 xl:grid-cols-4">
-          {achievements.map((achievement, index) => (
+          {displayedAchievements.map((achievement, index) => (
             <AchievementCard key={`profile-achievement-${index}`} data={achievement} />
           ))}
         </div>
-        <Link href={`/achievements/`} className="text-primary pt-4 flex items-center gap-4 cursor-pointer">
-        {" "}
-        <Plus />
-        <span>See all</span>
-        
-      </Link>
+        <button onClick={() => setShowAll(true)} className="text-primary bg-transparent pt-4.5 flex items-center gap-4 cursor-pointer">
+          {" "}
+          <Plus />
+          <span>See all</span>
+        </button>
       </ProfileOverviewSection>
     );
+  }
   return <></>;
 }
