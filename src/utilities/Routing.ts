d2fd80b75feaf12ead5a6404b6cbb190591f803b
musayann <musayann@gmail.com>
2023-10-27 14:20:57 +0200

    fix: localePath parsing

---
+++ src/utilities/Routing.ts
@@ -0,0 +1,11 @@
+import { NextRouter } from "next/router";
+
+// it doesn't matter here since were are just returning the path name
+const DEFAULT_DOMAIN = "https://dacade.org";
+
+export function localePath(router: NextRouter, path: string) {
+    const locale = router.locale !== router.defaultLocale ? router.locale : '';
+    const url = new URL(`/${locale}/${path}`, DEFAULT_DOMAIN);
+    console.log(url.pathname);
+    return url.pathname;
+}
