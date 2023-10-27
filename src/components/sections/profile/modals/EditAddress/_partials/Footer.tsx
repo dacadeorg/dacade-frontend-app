7acabf8459552c8772655a9d99c45f013c1b1edd
serapieTuyishime <tuyiserapie1@gmail.com>
2023-10-27 18:36:57 +0200

    refactor: add comments and update the form button text

--- src/components/sections/profile/modals/EditAddress/_partials/Footer.tsx
+++ src/components/sections/profile/modals/EditAddress/_partials/Footer.tsx
@@ -2,23 +2,34 @@ import ArrowButton from "@/components/ui/button/Arrow";
 import { useTranslation } from "react-i18next";
 
 type Props = {
-    actionButtonDisabled: boolean;
-    loading: boolean;
-    onClose: () => void
-}
+  actionButtonDisabled: boolean;
+  loading: boolean;
+  onClose: () => void;
+  isEditing: boolean;
+};
 
+/**
+ * Footer of the input address form
+ * @date 10/27/2023 - 5:48:42 PM
+ *
+ * @export
+ * @param {Props} param0
+ * @param {() => void} param0.onClose
+ * @param {boolean} param0.actionButtonDisabled
+ * @param {boolean} param0.loading
+ * @returns {*}
+ */
+export default function EditAdressFooter({ onClose, actionButtonDisabled, loading, isEditing }: Props) {
+  const { t } = useTranslation();
+  return (
+    <div className="flex items-center justify-between pt-4 pb-2 pl-6 pr-2 -mb-6">
+      <span className="text-sm font-medium cursor-pointer text-primary" onClick={onClose}>
+        {t("profile.edit.close")}
+      </span>
 
-export default function EditAdressFooter({ onClose, actionButtonDisabled, loading }: Props) {
-    const { t } = useTranslation();
-    return (
-        <div className="flex items-center justify-between pt-4 pb-2 pl-6 pr-2 -mb-6">
-            <span className="text-sm font-medium cursor-pointer text-primary" onClick={onClose}>
-                {t("profile.edit.close")}
-            </span>
-
-            <ArrowButton disabled={actionButtonDisabled} loading={loading}>
-                {t("profile.edit.wallet.button.save-address")}
-            </ArrowButton>
-        </div>
-    )
+      <ArrowButton disabled={actionButtonDisabled} loading={loading}>
+        {t(isEditing ? "profile.edit.wallet.button.change-address" : "profile.edit.wallet.button.save-address")}
+      </ArrowButton>
+    </div>
+  );
 }
--- src/components/sections/profile/modals/EditAddress/_partials/Info.tsx
+++ src/components/sections/profile/modals/EditAddress/_partials/Info.tsx
@@ -1,40 +1,52 @@
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
-
 type Props = {
-    address: string;
-    onClick: () => void;
-    connectionMethod: string;
-}
+  address: string;
+  onClick: () => void;
+  connectionMethod: string;
+};
 
+/**
+ * Display info of the wallet
+ * @date 10/27/2023 - 5:52:17 PM
+ *
+ * @export
+ * @param {Props} param0
+ * @param {string} param0.address
+ * @param {() => void} param0.onClick
+ * @param {string} param0.connectionMethod
+ * @returns {*}
+ */
 export function WalletInfo({ address, onClick, connectionMethod }: Props) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const newAddressTitle = useMemo(() => {
-        if (connectionMethod === "manual") return "Enter new address";
-        if (connectionMethod === "wallet") return "New address";
-        return "";
-    }, [connectionMethod]);
+  const newAddressTitle = useMemo(() => {
+    if (connectionMethod === "manual") return "Enter new address";
+    if (connectionMethod === "wallet") return "New address";
+    return "";
+  }, [connectionMethod]);
 
-    return (
-        <div className="flex flex-col space-y-3">
-            <div className="flex">
-                <p className="text-base font-medium">{t(address ? "profile.edit.wallet.current.address" : "profile.edit.wallet.input.label.manual")}</p>
+  return (
+    <div className="flex flex-col space-y-3">
+      <div className="flex">
+        <p className="text-base font-medium">{t(address ? "profile.edit.wallet.current.address" : "profile.edit.wallet.input.label.manual")}</p>
 
-                {address && (<span className="ml-auto text-base font-medium cursor-pointer text-primary" onClick={onClick}>
-                    {t("profile.edit.wallet.button.change")}
-                </span>)}
-            </div>
+        {address && (
+          <span className="ml-auto text-base font-medium cursor-pointer text-primary" onClick={onClick}>
+            {t("profile.edit.wallet.button.change")}
+          </span>
+        )}
+      </div>
 
-            {address && (
-                <>
-                    <p className="mb-3 text-base">{address}</p>
-                    <div className="pb-2">
-                        <p className="text-base font-medium">{newAddressTitle}</p>
-                    </div>
-                </>
-            )}
-        </div>
-    )
+      {address && (
+        <>
+          <p className="mb-3 text-base">{address}</p>
+          <div className="pb-2">
+            <p className="text-base font-medium">{newAddressTitle}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
