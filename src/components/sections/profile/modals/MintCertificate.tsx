041601be3d357530403e478c1eda64110e0f97c0
serapieTuyishime <tuyiserapie1@gmail.com>
2023-10-31 11:42:32 +0200

    refactor:comment out the  mintCertificate component to allow building

--- src/components/sections/profile/modals/MintCertificate.tsx
+++ src/components/sections/profile/modals/MintCertificate.tsx
@@ -2,13 +2,13 @@ import { useState, useEffect, useMemo, ReactElement } from "react";
 import Modal from "@/components/ui/Modal";
 import AchievementCard from "@/components/cards/Achievement";
 import ArrowButton from "@/components/ui/button/Arrow";
-import ErrorBox from "@/components/ui/ErrorBox";
+// import ErrorBox from "@/components/ui/ErrorBox";
 import Input from "@/components/ui/Input";
 import { useMultiSelector } from "@/hooks/useTypedSelector";
-import { useDispatch } from "@/hooks/useTypedDispatch";
+// import { useDispatch } from "@/hooks/useTypedDispatch";
 import { useTranslation } from "next-i18next";
-import { check, connectWallet, getSignature } from "@/store/feature/wallet.slice";
-import { mintCertificate } from "@/store/services/profile/certificate.service";
+// import { check, connectWallet, getSignature } from "@/store/feature/wallet.slice";
+// import { mintCertificate } from "@/store/services/profile/certificate.service";
 import { Certificate, Minting } from "@/types/certificate";
 import { IRootState } from "@/store";
 
@@ -48,17 +48,17 @@ interface Wallet {
  */
 export default function MintCertificate({ show, close }: { show: boolean; wallet?: Wallet; close?: (value: boolean) => void }): ReactElement {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<any>();
+  // const [error, setError] = useState<any>();
   const [txData, setTxData] = useState({ tx: "" });
-  const { achievement, walletAddress, mintingTx } = useMultiSelector<unknown, MintCertificateMultiSelector>({
+  const { achievement, walletAddress } = useMultiSelector<unknown, MintCertificateMultiSelector>({
     achievement: (state: IRootState) => state.profileCertificate.current,
     walletAddress: (state: IRootState) => state.web3Wallet.address,
     mintingTx: (state: IRootState) => state.certificates.mintingTx,
   });
 
-  const connected = check();
+  // const connected = check();
 
   // User wallet address
   const address = useMemo(() => walletAddress?.toLowerCase(), [walletAddress]);
@@ -100,34 +100,34 @@ export default function MintCertificate({ show, close }: { show: boolean; wallet
    * @async
    * @returns { () => Promise<void>}
    */
-  const onSave = async () => {
-    if (loading) return;
-    setLoading(true);
-    setError((prev: any) => ({ ...prev, message: "" }));
-    try {
-      const signature = await getSignature();
+  // const onSave = async () => {
+  //   if (loading) return;
+  //   setLoading(true);
+  //   setError((prev: any) => ({ ...prev, message: "" }));
+  //   try {
+  //     const signature = await getSignature();
 
-      if (signature && achievement?.id) {
-        await dispatch(
-          mintCertificate({
-            id: achievement.id as string,
-            address: address as string,
-            signature,
-          })
-        );
-        mintingTx && setTxData((prev) => ({ ...prev, tx: mintingTx.tx }));
-      }
-    } catch (error: any) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     if (signature && achievement?.id) {
+  //       await dispatch(
+  //         mintCertificate({
+  //           id: achievement.id as string,
+  //           address: address as string,
+  //           signature,
+  //         })
+  //       );
+  //       mintingTx && setTxData((prev) => ({ ...prev, tx: mintingTx.tx }));
+  //     }
+  //   } catch (error: any) {
+  //     setError(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   // Connect wallet function
   const connect = async () => {
     try {
-      await dispatch(connectWallet());
+      // await dispatch(connectWallet());
     } catch (error) {
       console.log(error);
     }
@@ -135,7 +135,7 @@ export default function MintCertificate({ show, close }: { show: boolean; wallet
 
   const handleSave = () => {
     if (!address) return connect();
-    onSave();
+    // onSave();
   };
 
   return (
@@ -150,7 +150,7 @@ export default function MintCertificate({ show, close }: { show: boolean; wallet
           </div>
 
           <div className="flex-1 overflow-hidden font-primary">
-            {error && <ErrorBox className="my-4" error={error} />}
+            {/* {error && <ErrorBox className="my-4" error={error} />} */}
             {minted ? (
               <div className="flex flex-col gap-3">
                 <div className="p-3 text-green-700 bg-green-100 border border-green-200 border-solid rounded">
@@ -169,7 +169,7 @@ export default function MintCertificate({ show, close }: { show: boolean; wallet
               <>
                 <p className="pt-3 pb-4">This certificate is awarded for passing the {achievement?.metadata?.name} course.</p>
                 {/* #TODO: this is not right, this connected is aways true and it should not */}
-                {!connected ? (
+                {true ? (
                   <div className="border-t border-gray-100 border-solid">
                     <p className="pt-4">Minting this certificate will not cost you gas fees.</p>
                     <Input value={address} placeholder="Wallet address" inputClass="h-12 mt-6 text-sm text-slate-500" fontSize="sm" required disabled />
