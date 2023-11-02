66cbe49fb33a88fb41ec878be9179b645eab412e
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2023-11-02 14:03:53 +0200

    fix: remove redundant tailwindcss classes

--- src/components/banner/PrivacyPolicy.tsx
+++ src/components/banner/PrivacyPolicy.tsx
@@ -41,7 +41,7 @@ export default function PrivacyPolicyBanner(): ReactElement {
           </Link>
         </div>
         <div
-          className="flex absolute lg:relative lg:p-6 md:py-0 lg:justify-center right-0 top-0 lg:top-0 lg:right-0 lg:items-center items-center"
+          className="flex absolute lg:relative lg:p-6 md:py-0 lg:justify-center right-0 top-0 lg:items-center items-center"
           onClick={onAcceptCookiesPolicy}
         >
           <div className="z-50 lg:h-8 h-7 lg:w-8 w-7 flex items-center text-white rounded-full lg:border-solid lg:border lg:border-white hover:bg-blue-700 bg-transparent cursor-pointer place-content-center">
