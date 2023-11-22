1fece5dd03054481ea85df4de254ca6dc668cf6e
mhenriette <mhenriette00@gmail.com>
2023-11-22 14:40:51 +0200

    feat: add challenge reward hackaton

--- src/components/sections/challenges/Rewards.tsx
+++ src/components/sections/challenges/Rewards.tsx
@@ -28,6 +28,14 @@ export function OverviewRewards(): ReactElement {
     return token.charAt(0).toUpperCase() + token.slice(1).toLowerCase();
   };
   const router = useRouter();
+  // TODO retrieve the challenge type and hackaton Reward properties once they are added from the backend.
+  const isHackaton = challenge.type === "hackathon";
+  const hackatonReward = {
+    totalReward: "$35K",
+    prize: "Prize Pool",
+    token: "ICP",
+    place: ["$300K", "$200K", "$100K"],
+  };
 
   return (
     <Section title={`${t("communities.overview.reward.title")}`}>
@@ -46,20 +54,24 @@ export function OverviewRewards(): ReactElement {
         </div>
         {rewards?.map((reward, index) => (
           <div key={`reward=${index}`} className="flex items-center">
-            <Coin size="medium" token={reward?.token} />
+            <Coin size="medium" token={isHackaton ? hackatonReward.token : reward?.token} />
             <div className="text-sm space-y-2 md:pl-2 max-w-max">
               <div className="flex gap-1 text-gray-700 font-medium">
-                <span>{totalReward}</span>
-                <span>{reward?.token}</span>
+                <span>{isHackaton ? hackatonReward.totalReward : totalReward}</span>
+                <span>{isHackaton ? hackatonReward.prize : reward?.token}</span>
                 <span>{t("communities.overview.challenge.rewards")}</span>
               </div>
               <div className="text-gray-400 text-xs font-medium leading-3 mt-1 flex">
-                {challenge?.rewards.map((reward, index) => (
-                  <span key={`reward-${index}`}>
-                    {index > 0 && "\u003B "}
-                    {reward.amount} {reward.token}/{formatToken(reward.type)}
-                  </span>
-                ))}
+                {isHackaton ? (
+                  <span>{`1st Place ${hackatonReward.place[0]};  2nd Place ${hackatonReward.place[1]}; 3rd Place ${hackatonReward.place[2]}`}</span>
+                ) : (
+                  challenge?.rewards.map((reward, index) => (
+                    <span key={`reward-${index}`}>
+                      {index > 0 && "\u003B "}
+                      {reward.amount} {reward.token}/{formatToken(reward.type)}
+                    </span>
+                  ))
+                )}
               </div>
             </div>
           </div>
