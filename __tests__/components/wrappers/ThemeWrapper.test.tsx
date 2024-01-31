68d08ccdb454b2281758e5654f8ea439ab6ac69d
Samantha-KY <mureraksamantha@gmail.com>
2024-01-31 12:09:22 +0200

    feat: add test for the theme wrapper

---
+++ __tests__/components/wrappers/ThemeWrapper.test.tsx
@@ -0,0 +1,21 @@
+import ThemeWrapper from "@/components/wrappers/ThemeWrapper";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { colors } from "../../../__mocks__/colors";
+
+describe("ThemeWrapper", () => {
+  it("should render the theme", () => {
+    render(<ThemeWrapper>Theme wrapper</ThemeWrapper>);
+    const themeWrapper = screen.getByTestId("themeWrapper");
+
+    expect(themeWrapper).toBeInTheDocument();
+    expect(themeWrapper.textContent).toBe("Theme wrapper");
+  });
+
+  it("should apply extra props", () => {
+    render(<ThemeWrapper colors={colors}>Theme wrapper</ThemeWrapper>);
+    const themeWrapper = screen.getByTestId("themeWrapper");
+
+    expect(themeWrapper.style.cssText).toContain("--tm-");
+  });
+});
