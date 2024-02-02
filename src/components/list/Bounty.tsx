0e14e54a5506630f640e07d7cdba5a36a8c521d2
Odile078 <ouwimpuhwe620@daviscollege.com>
2024-02-02 16:25:52 +0200

    refactor: rename bounty-card key for clarity

--- src/components/list/Bounty.tsx
+++ src/components/list/Bounty.tsx
@@ -31,7 +31,7 @@ interface BountyListProps {
  * @returns {ReactElement}
  */
 export default function BountyList({ bounties = [], referrals = [] }: BountyListProps): ReactElement {
-  const loading = useSelector((state: IRootState) => state.bounties.isLoading)
+  const loading = useSelector((state: IRootState) => state.bounties.isLoading);
   return (
     <div>
       {bounties && !loading ? (
@@ -40,12 +40,14 @@ export default function BountyList({ bounties = [], referrals = [] }: BountyList
             <Referral referral={referral} key={referral.name} />
           ))}
           {bounties.map((bounty, index) => (
-            <Bounty bounty={bounty} key={`${index}-${bounty.id}`} />
+            <Bounty bounty={bounty} key={`bounty-${index}-${bounty.id}`} />
           ))}
         </div>
-      ) : <div className="h-24 sm:h-48 grid place-items-center">
-        <Loader />
-      </div>}
+      ) : (
+        <div className="h-24 sm:h-48 grid place-items-center">
+          <Loader />
+        </div>
+      )}
     </div>
   );
 }
