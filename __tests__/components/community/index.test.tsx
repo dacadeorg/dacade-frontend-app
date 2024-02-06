38b15498c748e71030b1cb62f01284b01e6097c3
serapieTuyishime <tuyiserapie1@gmail.com>
2024-02-06 15:45:16 +0200

    chore: mock the router

--- __tests__/components/community/index.test.tsx
+++ __tests__/components/community/index.test.tsx
@@ -1,11 +1,13 @@
+import "@testing-library/jest-dom";
 import CommunityCard from "@/components/cards/community";
 import { render, screen } from "@testing-library/react";
 import { community } from "../../../__mocks__/community";
+import mockRouter from "next-router-mock";
 
-import "@testing-library/jest-dom";
-
+jest.mock("next/router", () => jest.requireActual("next-router-mock"));
 describe("CommunityCard", () => {
   it("should render the communit card", () => {
+    mockRouter.push("/communities");
     render(<CommunityCard showRewards={true} community={community} />);
     console.log("the community data", community);
     const communitycardContent = screen.getByTestId("community-card");
