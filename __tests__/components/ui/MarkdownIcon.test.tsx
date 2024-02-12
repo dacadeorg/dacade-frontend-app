ef6d913daa3f6721ff1bb09baed75d7121a0ea2e
Samantha-KY <mureraksamantha@gmail.com>
2024-02-12 15:07:16 +0200

    feat: add markDownIcon test

---
+++ __tests__/components/ui/MarkdownIcon.test.tsx
@@ -0,0 +1,20 @@
+import MarkdownIcon from "@/components/ui/MarkdownIcon";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("MarkdownIcon", () => {
+    it("should render the Markdown icon with the correct link", () => {
+      render(<MarkdownIcon />);
+  
+      // Check if the Markdown icon is rendered
+      const markdownIcon = screen.getByTestId("markdown-icon");
+      expect(markdownIcon).toBeInTheDocument();
+    });
+
+    it("should render the Markdown link", () => {
+        render(<MarkdownIcon />);
+        const markdownLink = screen.getByText("Markdown");
+      expect(markdownLink).toBeInTheDocument();
+      expect(markdownLink).toHaveAttribute("href", "https://www.markdownguide.org/cheat-sheet/");
+    })
+  });
