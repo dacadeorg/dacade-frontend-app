6ca906f2e6eb2f5b907334a1afb4e50fcb2c6ed8
Samantha-KY <mureraksamantha@gmail.com>
2024-02-16 12:23:51 +0200

    refactor: wrap with redux provider and mock register

--- __tests__/components/ui/UsernameInput.test.tsx
+++ __tests__/components/ui/UsernameInput.test.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import UsernameInput from "@/components/ui/UsernameInput";
 import "@testing-library/jest-dom";
-import { useForm } from "react-hook-form";
-import { FormValues } from "@/pages/signup";
+import ReduxProvider from "../../../__mocks__/provider/ReduxProvider";
+
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+    },
+    isFallback: false,
+  }),
+}));
 
 describe("UsernameInput", () => {
     it("should render the username input", ()=> {
-      const { register } = useForm<FormValues>()
-    render(<UsernameInput register={register} errors={{}} usernameValue=""/>)
-    })
+      const register = jest.fn();
+    render(
+      <ReduxProvider>
+    <UsernameInput register={register} errors={{}} usernameValue=""/>
+    </ReduxProvider>
+    )
     const usernameValue = screen.getByTestId("usernameInput")
 
     expect(usernameValue).toBeInTheDocument()
+    })
 })
+
