dd3778b1a63f9ad4d2f8fa409aea2eb2043a7bd7
Samantha-KY <mureraksamantha@gmail.com>
2024-03-05 17:29:59 +0200

    refactor: format code and remove repeated test

--- __tests__/components/ui/Currency.test.tsx
+++ __tests__/components/ui/Currency.test.tsx
@@ -3,22 +3,20 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 jest.mock("@coingecko/cryptoformat", () => ({
-    formatCurrency: jest.fn((value, token) => `${value} ${token}`),
-  }));
+  formatCurrency: jest.fn((value, token) => `${value} ${token}`),
+}));
 
 describe("Currency", () => {
-    it("should render currency", () => {
-        render(<Currency/>)
-        const currency = screen.getByTestId("currencyId")
-        expect(currency).toBeInTheDocument()
-        expect(currency).toHaveTextContent("0");
-    })
-
-    it("should render currency with value and token", () => {
-        render(<Currency value={100} token="USD" />);
+  it("should render currency", () => {
+    render(<Currency />);
     const currency = screen.getByTestId("currencyId");
     expect(currency).toBeInTheDocument();
-    expect(currency).toHaveTextContent("100 USD");
-    })
+    expect(currency).toHaveTextContent("0");
+  });
 
-})
+  it("should render currency with value and token", () => {
+    render(<Currency value={100} token="USD" />);
+    const currency = screen.getByTestId("currencyId");
+    expect(currency).toHaveTextContent("100 USD");
+  });
+});
