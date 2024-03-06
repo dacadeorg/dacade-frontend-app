5c6af129239c5b11086f966f949beac03eca9917
mhenriette <mhenriette00@gmail.com>
2024-03-06 18:11:55 +0200

    fix: unequal spacing on the bounty challenge

--- src/components/cards/Bounty.tsx
+++ src/components/cards/Bounty.tsx
@@ -58,7 +58,7 @@ export default function BountyCard({ bounty }: BountyProps): ReactElement {
 
   return (
     <div className="p-5 flex md:flex-row-reverse md:space-x-5 px-5 min-h-32 md:h-auto md:w-full justify-between hover:bg-secondary relative">
-      <div className="bg-theme-accent flex-col w-full h-full justify-between md:-space-y-1 pl-3 pr-5 mt-7 mb-5">
+      <div className="bg-theme-accent flex-col w-full h-full justify-between md:-space-y-1 pl-3 pr-5 my-5">
         <Component className="relative w-full block" href={link}>
           <div className="font-medium text-md md:pt-1.5">{bounty.course ? bounty.course.name : bounty.name}</div>
         </Component>
