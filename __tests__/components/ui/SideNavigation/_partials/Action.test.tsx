8d15d4e73fb6535d05cc73db130a4bf8c866c328
Samantha-KY <mureraksamantha@gmail.com>
2024-03-06 14:49:10 +0200

    feat: add sidenavigation partial action test

---
+++ __tests__/components/ui/SideNavigation/_partials/Action.test.tsx
@@ -0,0 +1,53 @@
+import { ActivableLink } from "@/components/ui/SideNavigation/_partials/Action";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("Activable link", () => {
+    const item = {
+        subitems: [],
+        link: "/example",
+    }
+    it("should render the link with subitems", () => {
+        render(<ActivableLink item={{
+            subitems: [{label: "subitem", link: "subitem link"}],
+            link: "/dummy"
+        }}>
+            Link test
+            </ActivableLink>)
+        const link = screen.getByTestId("activableLinkId")
+        expect(link).toBeInTheDocument()
+        expect(link.textContent).toBe("Link test")
+    })
+
+    it("should render the link with no subitems", () => {
+       
+        render(<ActivableLink item={item}>Link test</ActivableLink>)
+        const link = screen.getByText("Link test")
+        expect(link.tagName).toBe("A");
+        expect(link).toHaveAttribute("href", item.link);
+    })
+
+    it("should render the link with activable link style", () => {
+        render(<ActivableLink item={{
+            subitems: [{label: "subitem", link: "subitem link"}],
+            link: "/dummy"
+        }} isActive>
+            Link test
+            </ActivableLink>)
+        const link = screen.getByTestId("activableLinkId")
+        expect(link.children[0].tagName).toBe("SPAN")
+        expect(link.children[0].className).toContain("activable-link")
+    })
+
+    it("should call goToLink function when clicked", () => {
+        const goToLinkMock = jest.fn();
+        render(<ActivableLink item={{
+            subitems: [{label: "subitem", link: "subitem link"}],
+            link: "/dummy"
+        }} goToLink={goToLinkMock}>Link Text</ActivableLink>);
+        const link = screen.getByTestId("activableLinkId");
+        fireEvent.click(link.children[0])
+        expect(goToLinkMock).toHaveBeenCalledTimes(1); 
+      });
+
+})
