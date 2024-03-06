0c65087c9db8b413632de803fb2953e6f61cfcab
Samantha-KY <mureraksamantha@gmail.com>
2024-03-06 11:31:47 +0200

    refactor: remove repeated test and use already existing mocked data

--- __tests__/components/ui/Certificate.test.tsx
+++ __tests__/components/ui/Certificate.test.tsx
@@ -1,16 +1,8 @@
 import Certificate from "@/components/ui/Certificate";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import ReactComponent from "../../../__mocks__/svg";
 
-jest.mock("@/icons/certificates/Celo.svg", () => "CeloIcon");
-jest.mock("@/icons/certificates/Algoland.svg", () => "AlgolandIcon");
-jest.mock("@/icons/certificates/Blockchain.svg", () => "BlockChainIcon");
-jest.mock("@/icons/certificates/Eternity.svg", () => "EthIcon");
-jest.mock("@/icons/certificates/Near.svg", () => "NearIcon");
-jest.mock("@/icons/certificates/Icp.svg", () => "IcpIcon");
-jest.mock("@/icons/certificates/Ai.svg", () => "AiIcon");
-jest.mock("@/icons/certificates/Solidity.svg", () => "SolidityIcon");
-jest.mock("@/icons/certificates/Sui.svg", () => "SuiIcon");
 
 describe("Certificate", () => {
     it("should render certificate", () => {
@@ -22,7 +14,6 @@ describe("Certificate", () => {
     it("should render the correct certificate icon based on the name", () => {
         const { getByTestId } = render(<Certificate name="sui" />);
         const certificate = getByTestId("certificateId");
-        expect(certificate).toBeInTheDocument();
-        expect(certificate.innerHTML).toContain("<suiicon></suiicon>");
+        expect(certificate.innerHTML).toContain(ReactComponent);
       });
 })
