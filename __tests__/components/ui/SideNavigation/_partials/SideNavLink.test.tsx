719d1966fc46e43d077225f98b636fb540998b31
Samantha-KY <mureraksamantha@gmail.com>
2024-03-11 16:04:27 +0200

    feat: add sideNavLink test

---
+++ __tests__/components/ui/SideNavigation/_partials/SideNavLink.test.tsx
@@ -0,0 +1,35 @@
+import CourseLink from "@/components/ui/SideNavigation/_partials/SideNavLink";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import ReduxProvider from "../../../../../__mocks__/provider/ReduxProvider";
+
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+    },
+    isFallback: false,
+  }),
+}));
+
+
+describe("SideNavLink", () => {
+    const item = {
+        link: "/example",
+        exact: true,
+        subitems: [],
+        label: "Example label"
+    }
+    it("should render side navigation links", () => {
+        render(
+        <ReduxProvider>
+        <CourseLink item={item}/>
+        </ReduxProvider>
+        )
+        const courseLink = screen.getByTestId("courseLinkId")
+        expect(courseLink).toBeInTheDocument()
+    })
+})
