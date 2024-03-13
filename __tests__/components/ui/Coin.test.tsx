9bc9b474cdf00d8b42d2ec6ffe1ba23a2a87169b
serapieTuyishime <tuyiserapie1@gmail.com>
2024-03-13 11:49:00 +0200

    fix: test whether the coin has svg as a component

--- __tests__/components/ui/Coin.test.tsx
+++ __tests__/components/ui/Coin.test.tsx
@@ -3,9 +3,29 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import fs from "fs";
 import path from "path";
+import DACIcon from "@/icons/tokens/DAC.svg";
+
 
 describe("Coin", () => {
-  it("should render the coin", () => {
+  it("should render a small size coin", () => {
+    render(<Coin size="small" />)
+    const coin = screen.getByTestId('coin')
+    expect(coin.className).toContain('w-4 h-4')
+  })
+
+  it("should render a coin with a specified shape", () => {
+    render(<Coin shape="rounded" />)
+    const coin = screen.getByTestId('coin')
+    expect(coin.className).toContain('rounded-xl')
+  })
+
+  it("should render a child inside of it", () => {
+    render(<Coin shape="rounded" />)
+    const coin = screen.getByTestId('coin')
+    expect(coin.childElementCount).toBeGreaterThanOrEqual(1)
+  })
+
+  it("should not render the DAC coin when a different token was passed", () => {
     const coinTokensDir = path.resolve(__dirname, "..", "..", "../src/icons/tokens/");
     const coinTokenFiles = fs.readdirSync(coinTokensDir);
 
@@ -13,10 +33,12 @@ describe("Coin", () => {
       const tokenName = token.split(".")[0];
       if (tokenName.trim() !== "") {
         render(<Coin token={tokenName} />);
-        const coins = screen.getAllByTestId("coin");
-        console.log("coins", coins);
+        const coin = screen.getByTestId("coin");
+        const icon = <DACIcon />
+        expect(coin).toBe(icon)
+        console.log("coins", coin);
         //    expect(coins).toBe(coinTokenFiles.length);
       }
-    });
-  });
+    })
+  })
 });
