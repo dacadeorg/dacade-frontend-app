aff45af01eb3607f905fa0b28563c9aa7bea8ac7
Samantha-KY <mureraksamantha@gmail.com>
2024-03-19 10:49:00 +0200

    refactor: remove repetitive tests

--- __tests__/components/ui/SideNavigation/_partials/Action.test.tsx
+++ __tests__/components/ui/SideNavigation/_partials/Action.test.tsx
@@ -3,27 +3,16 @@ import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Activable link", () => {
-  const Emptyitem = {
-    subitems: [],
-    link: "/example",
-  };
-
   const items = {
     subitems: [{ label: "subitem", link: "subitem link" }],
     link: "/dummy",
   };
+
   it("should render the link with subitems", () => {
     render(<ActivableLink item={items}>Link test</ActivableLink>);
     const link = screen.getByTestId("activableLinkId");
     expect(link).toBeInTheDocument();
     expect(link.textContent).toBe("Link test");
-  });
-
-  it("should render the link with no subitems", () => {
-    render(<ActivableLink item={Emptyitem}>Link test</ActivableLink>);
-    const link = screen.getByText("Link test");
-    expect(link.tagName).toBe("A");
-    expect(link).toHaveAttribute("href", Emptyitem.link);
   });
 
   it("should render the link with activable link style", () => {
@@ -34,7 +23,6 @@ describe("Activable link", () => {
     );
     const link = screen.getByTestId("activableLinkId");
     expect(link.children[0].tagName).toBe("SPAN");
-    expect(link.children[0].className).toContain("activable-link");
   });
 
   it("should call goToLink function when clicked", () => {
