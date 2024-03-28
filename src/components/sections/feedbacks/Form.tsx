a1fd9c785d937b5d0da42256f58ef13791016b59
serapieTuyishime <tuyiserapie1@gmail.com>
2024-03-28 10:20:30 +0200

    refactor: cleanup uneeded test ids

--- src/components/sections/feedbacks/Form.tsx
+++ src/components/sections/feedbacks/Form.tsx
@@ -122,7 +122,6 @@ export default function Form({ onSave }: FormProps): ReactElement {
           </div>
           <div label-for="input-text">
             <TextInput
-              data-testid="input-text"
               id="input-text"
               placeholder={t("communities.challenge.submission.feedback.placeholder.text") || ""}
               inputClass="border-t-0"
