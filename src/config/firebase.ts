7360bbbb97bfd12b84360946fb8743c9fdc4b8c2
Samantha-KY <mureraksamantha@gmail.com>
2024-04-03 10:46:37 +0200

    feat: add mock string

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,11 +8,11 @@ let analytics: Analytics;
 
 if (typeof window !== "undefined") {
   firebase = initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "mock-key",
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mock-project",
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mock-auth",
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "mock-app",
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "mock-measurement",
   });
 
   auth = getAuth(firebase);
