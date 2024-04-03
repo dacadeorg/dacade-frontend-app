7fcd4f62d4a8ba0891d9f72deb05d989c9f15f13
Samantha-KY <mureraksamantha@gmail.com>
2024-04-03 10:37:20 +0200

    feat: add mock key

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,7 +8,7 @@ let analytics: Analytics;
 
 if (typeof window !== "undefined") {
   firebase = initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "mock_key",
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
