a9cd07bf526c38ee4c075dea90101664d92f9fdc
Samantha-KY <mureraksamantha@gmail.com>
2024-04-24 13:32:25 +0200

    feat: use element as the argument

--- __mocks__/renderWithRedux.tsx
+++ __mocks__/renderWithRedux.tsx
@@ -2,8 +2,8 @@ import { RenderOptions, render } from "@testing-library/react";
 import ReduxProvider from "./provider/ReduxProvider";
 import { ReactElement } from "react";
 
-const customRender = (ui: ReactElement , options?: Omit<RenderOptions, "wrapper">) => {
-    return render(ui, {wrapper: ReduxProvider, ...options})
+const customRender = (element: ReactElement , options?: Omit<RenderOptions, "wrapper">) => {
+    return render(element, {wrapper: ReduxProvider, ...options})
 }
 
 export * from "@testing-library/react"
