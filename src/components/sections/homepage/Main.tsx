1506d632f15d53846ec9d9b69edcfdbbfb6e4b11
mhenriette <mhenriette00@gmail.com>
2024-04-26 16:24:58 +0200

    fix: remove arbitrary value

--- src/components/sections/homepage/Main.tsx
+++ src/components/sections/homepage/Main.tsx
@@ -41,7 +41,7 @@ export default function MainSection(): ReactElement {
     <Section type="default" padding="p-0">
       <div className="w-full lg:py-24 pt-24 pb-8">
         <h1 className="lg:text-8xl md:text-6xl text-5xl leading-none tracking-tighter">{t("page.index.main.title")}</h1>
-        <p className="lg:text-8xl md:text-6xl text-[40px] py-2 lg:py-0 md:py-0 sm:py-0 text-gray-400 tracking-tighter leading-none">
+        <p className="lg:text-8xl md:text-6xl text-4.5xl py-2 lg:py-0 md:py-0 sm:py-0 text-gray-400 tracking-tighter leading-none">
           {t("page.index.main.subtitle")}
         </p>
       </div>
