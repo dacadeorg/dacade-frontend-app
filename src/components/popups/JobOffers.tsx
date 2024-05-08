c06e3d6af147434346f113774683e399115a97f2
Odile078 <ouwimpuhwe620@daviscollege.com>
2024-05-08 16:01:24 +0200

    feat: add translations for jobOffers component

--- src/components/popups/JobOffers.tsx
+++ src/components/popups/JobOffers.tsx
@@ -5,6 +5,8 @@ import Popup from "../ui/Popup";
 import Button from "../ui/button";
 import CloseIcon from "@/icons/close-icon.svg";
 import JobIcon from "@/icons/briefcase.svg";
+import { JOBOFFERSFORMLINK } from "@/constants/jobOffers";
+import { useTranslation } from "react-i18next";
 
 /**
  * JobOffers interface props
@@ -25,6 +27,8 @@ interface JobOffersProps {
  * @returns {*}
  */
 export default function JobOffers({ externalClick }: JobOffersProps) {
+  const { t } = useTranslation();
+
   const [show, setShow] = useState(false);
 
   const dispatch = useDispatch();
@@ -42,24 +46,24 @@ export default function JobOffers({ externalClick }: JobOffersProps) {
 
   return (
     <>
-      <button className="hidden lg:inline-block underline text-primary bg-transparent pl-2 text-base capitalize" onClick={onOpenJobOffers}>
-        Job offers
+      <button className="hidden lg:inline-block underline text-primary bg-transparent pl-2 py-0 text-base capitalize " onClick={onOpenJobOffers}>
+        {t("job.offers.title")}
       </button>
       <div className="flex lg:hidden">
-        <div className="w-10 h-10 ml-3 mr-2 my-3 rounded-full bg-blue-500">
+        <div className="w-10 h-10 ml-3 mr-2 my-3 rounded-full bg-primary">
           <JobIcon className="m-2 text-white" />
         </div>
         <div className="py-5 font-medium text-gray-900 cursor-pointer" onClick={onOpenJobOffers}>
-          <p className="font-medium text-lg text-gray-900 capitalize">Job offers</p>
+          <p className="font-medium text-lg text-gray-900 capitalize"> {t("job.offers.title")}</p>
         </div>
       </div>
-      <Popup center show={show} className="px-3 pt-16 pb-2" overlayClassName="bg-white opacity-80" onClose={onClose}>
+      <Popup center show={show} className="px-3" overlayClassName="bg-white opacity-80" onClose={onClose}>
         <div className="max-h-full overflow-scroll md:max-w-sidebar relative mx-auto mt-0 w-full z-40 bg-white rounded-3xl shadow-3xl text-gray-900 px-8 py-6 lg:min-w-98">
           <div className="space-y-6">
-            <h5 className="text-lg font-medium">Job offers</h5>
-            <p className="text-base">Interested in potential job opportunities? Fill in this google form and we’ll contact you for relevant roles.</p>
-            <Button padding link="https://forms.gle/czmMN9L5Hh88aUQu6" target="blank" className="py-2 px-6">
-              Next
+            <h5 className="text-lg font-medium"> {t("job.offers.title")}</h5>
+            <p className="text-base"> {t("job.offers.description")}</p>
+            <Button padding link={JOBOFFERSFORMLINK} target="blank" className="py-2 px-6">
+              {t("job.offers.button.next")}
             </Button>
           </div>
           <div className="absolute top-5 right-5 w-fit h-fit">
---
+++ src/constants/jobOffers.ts
@@ -0,0 +1,1 @@
+export const JOBOFFERSFORMLINK = "https://forms.gle/czmMN9L5Hh88aUQu6";
