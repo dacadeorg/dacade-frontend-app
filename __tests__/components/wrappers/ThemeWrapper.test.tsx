59e56052446a9068d80051ccabafe9557f32aa5d
serapieTuyishime <tuyiserapie1@gmail.com>
2024-05-16 12:24:37 +0200

    refactor: remove testing of the styles on the theme wrapper

--- __tests__/components/wrappers/ThemeWrapper.test.tsx
+++ __tests__/components/wrappers/ThemeWrapper.test.tsx
@@ -1,21 +1,13 @@
 import ThemeWrapper from "@/components/wrappers/ThemeWrapper";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import { colors } from "../../../__mocks__/colors";
 
 describe("ThemeWrapper", () => {
-  it("should render the theme", () => {
+  it("should render the theme wrapper", () => {
     render(<ThemeWrapper>Theme wrapper</ThemeWrapper>);
     const themeWrapper = screen.getByTestId("themeWrapper");
 
     expect(themeWrapper).toBeInTheDocument();
     expect(themeWrapper.textContent).toBe("Theme wrapper");
-  });
-
-  it("should apply extra props", () => {
-    render(<ThemeWrapper colors={colors}>Theme wrapper</ThemeWrapper>);
-    const themeWrapper = screen.getByTestId("themeWrapper");
-
-    expect(themeWrapper.style.cssText).toContain("--tm-");
   });
 });
