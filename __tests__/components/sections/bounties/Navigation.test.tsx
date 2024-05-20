929f081a39d28194883f0f63d41162f7c1c8607b
Samantha-KY <mureraksamantha@gmail.com>
2024-05-20 12:11:21 +0200

    feat: add missing test for the navigation

--- __tests__/components/sections/bounties/Navigation.test.tsx
+++ __tests__/components/sections/bounties/Navigation.test.tsx
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 import { screen } from "@testing-library/react";
 import BountiesNavigation from "@/components/sections/bounties/Navigation";
 import { renderWithRedux } from "../../../../__mocks__/renderWithRedux";
+import { List } from "@/utilities/CommunityNavigation";
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn().mockImplementation(() => ({
@@ -16,7 +17,19 @@ jest.mock("next/router", () => ({
   })),
 }));
 
-const expectedNavItems = ["bounties.navigation.all", "Bounties"];
+// const expectedNavItems = ["bounties.navigation.all", "Bounties"];
+const expectedNavItems: Omit<List, "id">[] = [
+  {
+    title: "bounties.navigation",
+    items: [
+      {
+        label: "bounties.navigation.all",
+        exact: true,
+        link: "/bounties",
+      },
+    ],
+  },
+];
 
 describe("Navigation", () => {
   it("should render the navigation", () => {
@@ -28,14 +41,33 @@ describe("Navigation", () => {
   it("should render menu items", () => {
     renderWithRedux(<BountiesNavigation />);
     const menuItems = screen.getAllByRole("listitem");
-    expect(menuItems.length).toBeGreaterThan(0);
-    expect(menuItems).toHaveLength(expectedNavItems.length);
+    menuItems.forEach(() => {
+      expectedNavItems.forEach((menu) => {
+        if (!menu.hideTitle) {
+          const menuTitle = menu.title;
+          expect(screen.getByText(menuTitle)).toBeInTheDocument();
+        }
+      });
+    });
   });
+
   it("renders links with href", () => {
     renderWithRedux(<BountiesNavigation />);
     const links = screen.getAllByRole("link");
     links.forEach((link) => {
       expect(link).toHaveAttribute("href");
+    });
+  });
+
+  it("renders the item label", () => {
+    renderWithRedux(<BountiesNavigation />);
+    const menuItems = screen.getAllByRole("listitem");
+    menuItems.forEach(() => {
+      expectedNavItems.forEach((item) => {
+        const itemLabel = item.items.find((item) => item.label);
+        const label = itemLabel?.label;
+        expect(screen.getByText(label || "")).toBeInTheDocument();
+      });
     });
   });
 });
