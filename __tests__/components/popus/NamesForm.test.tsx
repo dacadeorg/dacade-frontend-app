c43efdbcb7fdd667c0d2bf4f7eefc1db41965b1d
serapieTuyishime <tuyiserapie1@gmail.com>
2024-05-20 18:58:00 +0200

    chore: test edit profile names form

--- __tests__/components/popus/NamesForm.test.tsx
+++ __tests__/components/popus/NamesForm.test.tsx
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom"
 import { renderWithRedux } from "../../../__mocks__/renderWithRedux"
 import EditProfile from "@/components/popups/profile-settings/NamesForm"
-import { screen } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
 
 jest.mock("next/router", () => ({
     useRouter: () => ({
@@ -16,10 +16,33 @@ jest.mock("next/router", () => ({
     }),
 }));
 const handleClose = jest.fn()
+jest.mock("../../../src/components/ui/button/Arrow", () => {
+    return {
+        __esModule: true,
+        default: () => {
+            return <div />;
+        },
+    };
+});
+beforeEach(() => {
+    renderWithRedux(<EditProfile show onClose={handleClose} />)
+})
 describe("NamesForm", () => {
     it("should render the names edit form", () => {
-        renderWithRedux(<EditProfile show onClose={handleClose} />)
         const form = screen.getByTestId('names-edit-form')
         expect(form).toBeInTheDocument()
+    })
+
+    it("should render the form with 2 inputs", () => {
+        const form = screen.getByTestId('names-edit-form')
+        const firstNameInput = form.querySelectorAll("div [data-testid='first-name']")
+        const lastNameInput = form.querySelectorAll("div [data-testid='last-name']")
+        expect(firstNameInput).toHaveLength(1)
+        expect(lastNameInput).toHaveLength(1)
+    })
+    it("it should close the modal when its open", () => {
+        expect(screen.getByTestId('names-edit-form')).toBeInTheDocument()
+        fireEvent.click(screen.getByTestId('close-button'))
+        expect(handleClose).toHaveBeenCalled()
     })
 })
