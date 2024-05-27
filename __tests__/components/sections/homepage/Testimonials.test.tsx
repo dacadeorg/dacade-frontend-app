dbb0e5f4199fdd05ebcbf9fba16498d61e864f7b
Samantha-KY <mureraksamantha@gmail.com>
2024-05-27 10:32:02 +0200

    feat: add more test for testimonials component

--- __tests__/components/sections/homepage/Testimonials.test.tsx
+++ __tests__/components/sections/homepage/Testimonials.test.tsx
@@ -1,12 +1,27 @@
-import "@testing-library/jest-dom";
-import { screen } from "@testing-library/react";
-import { renderWithRedux } from "../../../../__mocks__/renderWithRedux";
+import { render, screen } from "@testing-library/react";
 import TestimonialsSection from "@/components/sections/homepage/Testimonials";
 
-describe("OpenSource", () => {
-    it("should render Open Source", () => {
-        renderWithRedux(<TestimonialsSection/>)
-        expect(screen.getByText("testimonials.community.title")).toBeInTheDocument()
-        expect(screen.getByText("footer.open.source")).toBeInTheDocument()
-     })
+describe("Testimonials Section", () => {
+  const testimonials = [
+    {
+      icon: "/assets/img/testimonials/CED.jpg",
+      content:
+        "Dacade helped me to meet people on the same blockchain journey as me and gives access to cool mentors! I like that it rewards you for challenges and helping others.",
+    },
+    {
+      icon: "/assets/img/testimonials/alex.jpg",
+      content: "Testimonials text",
+    },
+  ];
+
+  it("should render testimonials with correct content", () => {
+    render(<TestimonialsSection />);
+    expect(screen.getByText("testimonials.community.title")).toBeInTheDocument();
+    expect(screen.getByText("footer.open.source")).toBeInTheDocument();
+
+    testimonials.forEach((testimonial) => {
+      expect(screen.getByAltText(testimonial.icon)).toBeInTheDocument();
+      expect(screen.getByText(testimonial.content)).toBeInTheDocument();
+    });
   });
+});
