83a00fc79d380e4a018094f14652b8b36df9c3d9
mhenriette <mhenriette00@gmail.com>
2024-06-03 16:27:22 +0200

    fix: add explicity type button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -101,7 +101,7 @@ export default function Navbar({ settings, sidebarBurgerColor = false }: NavbarP
           <ul className="hidden lg:block relative self-center">
             <NavItem to="/bounties">{t("nav.bounties")}</NavItem>
             <NavItem to={"/communities"}>{t("nav.communities")}</NavItem>
-            <button className="nav-item underline underline-offset-2 text-primary bg-transparent p-0 capitalize" onClick={displayJobOffersPopup}>
+            <button type="button" className="nav-item underline underline-offset-2 text-primary bg-transparent p-0 capitalize" onClick={displayJobOffersPopup}>
               {t("job.offers.title")}
             </button>
           </ul>
