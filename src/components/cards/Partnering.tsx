2645cc8e1ed60b50000b2e06ffd724a31c2a61c8
mhenriette <mhenriette00@gmail.com>
2024-06-10 18:02:07 +0200

    refactor: partnering card

--- src/components/cards/Partnering.tsx
+++ src/components/cards/Partnering.tsx
@@ -10,27 +10,25 @@ import { useTranslation } from "next-i18next";
  */
 export default function PartneringCard() {
   const { t } = useTranslation();
-  return (
-    <div className="relative lg:mt-0 xl:pt-0 rounded-3.5xl overflow-hidden mt-5 md:mt-0 border border-solid border-gray-light md:border-none">
-      <div className="relative z-10 p-6 lg:pb-3 w-full text-primary h-full min-h-xs flex flex-col justify-between divide-y-2 divide-y divide-dotted divide-theme-accent">
-        <div className="max-w-sm">
-          <p className="text-2xl font-medium mb-2.5 leading-none">{t("page.index.communities.partnering.title")}</p>
-          <p className="text-base mt-0 mb-20 lg:mb-0 leading-normal">{t("page.index.communities.partnering.subtitle")}</p>
-        </div>
-        <div className="mt-4 flex-none md:flex justify-between">
-          <div className="md:flex md:flex-col space-y-0">
-            <div className="mt-4 font-light text-theme-accent md:w-48">{t("partnering.card.text")}</div>
+      return (
+        <div className="relative z-10 mt-6 md:mt-0 p-6 rounded-3.5xl border border-solid bg-surface-bg-highlight lg:pb-3 w-full text-surface-text-brand h-full min-h-xs flex flex-col justify-between divide-y-2 divide-dotted divide-theme-accent">
+          <div className="max-w-sm">
+            <p className="text-2xl font-medium mb-2.5 leading-none">{t("page.index.communities.partnering.title")}</p>
+            <p className="text-base mt-0 mb-20 lg:mb-0 leading-normal">{t("page.index.communities.partnering.subtitle")}</p>
           </div>
-          <div className="mt-4 align-middle">
-            <a href="mailto:moritz@dacade.org">
-              <Button className="hover:bg-primary group-hover:text-white leading-relaxed lg:px-7 px-5 font-medium" variant="outline-primary" type="button" padding={false}>
-                {t("page.index.communities.partnering.button-text")}
-              </Button>
-            </a>
+          <div className="mt-4 flex-none md:flex justify-between">
+            <div className="md:flex md:flex-col space-y-0">
+              <div className="mt-4 font-light text-theme-accent md:w-48">{t("partnering.card.text")}</div>
+            </div>
+            <div className="mt-4 align-middle">
+              <a href="mailto:moritz@dacade.org">
+                <Button className="hover:bg-primary group-hover:text-white leading-relaxed lg:px-7 px-5 font-medium" variant="outline-primary" type="button" padding={false}>
+                  {t("page.index.communities.partnering.button-text")}
+                </Button>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <span className="block absolute top-0 left-0 bg-primary opacity-5 z-0 w-full h-full" />
-    </div>
-  );
+      );
+
 }
