99c1b43fdffa2ca97ae86b62b77e15258ff50f99
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2024-06-11 14:19:36 +0200

    fix: disclaimer component test

--- __tests__/components/sections/courses/Overview/Disclaimer.test.tsx
+++ __tests__/components/sections/courses/Overview/Disclaimer.test.tsx
@@ -1,34 +1,15 @@
 import "@testing-library/jest-dom";
 import Disclaimer from "@/components/sections/courses/overview/Disclaimer";
 import { render, screen } from "@testing-library/react";
-// import { course } from "../../__mocks__/course.ts";
-import ReduxProvider from "../../../../../__mocks__/provider/ReduxProvider";
-jest.mock("next/router", () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-    isFallback: false,
-  }),
-}));
 
 
-const RenderDisclaimer = () => {
-  render(
-    <ReduxProvider>
-      <Disclaimer />
-    </ReduxProvider>
-  );
-  return screen.getByTestId("disclaimer-show");
-}
-
-describe("Disclaimer", () => {
   it("should render the Disclaimer", () => {
-    const disclaimerComponent = RenderDisclaimer();
+    render(  <Disclaimer />);
+    const disclaimerComponent = screen.getByTestId("disclaimerId");;
     expect(disclaimerComponent).toBeInTheDocument();
   });
 
   it("should show disclaimer HTML", () => {
-    RenderDisclaimer();
-    const courseDisclaimer = screen.getByTestId("disclaimer");
+    const courseDisclaimer = screen.getByTestId("disclaimerSpanId");
     expect(courseDisclaimer).toBeInTheDocument();
   });
-});
