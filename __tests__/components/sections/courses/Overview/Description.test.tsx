25124255ddae4d0bf04737889bb40d3d56ef1a01
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2024-06-11 14:10:06 +0200

    fix: description component test

--- __tests__/components/sections/courses/Overview/Description.test.tsx
+++ __tests__/components/sections/courses/Overview/Description.test.tsx
@@ -1,29 +1,11 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Description from "@/components/sections/courses/overview/Description";
-// import { course } from "../../__mocks__/course.ts";
-import ReduxProvider from "../../../../../__mocks__/provider/ReduxProvider";
 
-jest.mock("next/router", () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-    isFallback: false,
-  }),
-}));
 
-const RenderDescription = () => {
-  render(
-    <ReduxProvider>
-      <Description />
-    </ReduxProvider>
-  );
-  return screen.getByTestId("description-show");
-}
-
-describe("Description", () => {
-  it("should render the Description", () => {
-    const description = RenderDescription();
-    expect(description).toBeInTheDocument();
-  });
-
+it("should render the Description", () => {
+  render( <Description />);
+  const description = screen.getByTestId("descriptionId");;
+  expect(description).toBeInTheDocument();
 });
+
