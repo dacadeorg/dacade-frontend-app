a6fc86f6683a0857bbeb89e3155935acd8d29107
gabrielaacha <gabriela.gabriela.acha.acha@gmail.com>
2024-06-11 15:43:50 +0200

    fix: trailer component test

--- __tests__/components/sections/courses/Overview/Trailer.test.tsx
+++ __tests__/components/sections/courses/Overview/Trailer.test.tsx
@@ -2,29 +2,58 @@
 import "@testing-library/jest-dom";
 import Trailer from "@/components/sections/courses/overview/Trailer";
 import { render, screen } from "@testing-library/react";
-// import { course } from "../../__mocks__/course.ts";
-import ReduxProvider from "../../../../../__mocks__/provider/ReduxProvider";
-
-jest.mock("next/router", () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-    isFallback: false,
-  }),
-}));
+import { mockCourse } from "../../../../../__mocks__/course";
+import ObjectiveList from "@/components/sections/courses/overview/Trailer";
+import Video from "@/components/sections/courses/overview/Trailer";
+import Duration from "@/components/sections/courses/overview/Trailer";
 
 
-const RenderTrailer = () => {
-  render(
-    <ReduxProvider>
-      <Trailer />
-    </ReduxProvider>
-  );
-  return screen.getByTestId("trailer-show");
+
+interface ObjectiveListProps {
+  objectives: [];
 }
+
+
+const RenderObjectiveList = (props?: ObjectiveListProps) => {
+  render(<ObjectiveList {...props} objectives={mockCourse.trailer?.info?.items}/>);
+  return screen.getByTestId("objectivesListShow");
+};
+
 
 describe("Trailer", () => {
   it("should render the Trailer", () => {
-    const trailer = RenderTrailer();
+    render(<Trailer />)
+    const trailer = screen.getByTestId("trailerId");
     expect(trailer).toBeInTheDocument();
   });
+
+  it("should render the Video", () => {
+    render(<Video />)
+    const trailerVideo = screen.getByTestId("trailerVideoId");
+    expect(trailerVideo).toBeInTheDocument();
+  });
+
+  it("should render the Duration", () => {
+    render(<Duration />)
+    const trailerDuration = screen.getByTestId("trailerDurationId");
+    expect(trailerDuration).toBeInTheDocument();
+  });
+
+  it("should render the Video Summary", () => {
+    const trailerVideoSummary = screen.getByTestId("trailerVideoSummary");
+    expect(trailerVideoSummary).toBeInTheDocument();
+    expect(trailerVideoSummary).toBe(mockCourse.trailer.description);
+  });
+
+  it("should render the Objective List", () => {
+    const objectiveList = RenderObjectiveList();
+    expect(objectiveList).toBeInTheDocument();
+  });
+
+  it("should render course trailer description", () => {
+    const trailerText = screen.getByTestId("trailerTextId");
+    expect(trailerText).toBeInTheDocument();
+    expect(trailerText).toBe(mockCourse.trailer.description);
+  });
+
 });
