33ed851d987a5972f6179b24443898054c3d377c
mhenriette <mhenriette00@gmail.com>
2024-06-19 17:39:13 +0200

    refactor: duration badge

--- src/components/badges/DurationBadge.tsx
+++ src/components/badges/DurationBadge.tsx
@@ -1,7 +1,7 @@
 import DateManager from "@/utilities/DateManager";
 import classNames from "classnames";
 import { useRouter } from "next/router";
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 
 interface DurationBadgeProps {
   value: number;
@@ -10,14 +10,13 @@ interface DurationBadgeProps {
 
 export default function DurationBadge({ value, type = "gray" }: DurationBadgeProps) {
   const router = useRouter();
+  const getDuration = useCallback((value: number, locale: string) => {
+    if (!value) return 0;
+    return DateManager.humanize(value, locale);
+  }, []);
   const duration = useMemo(() => {
-    return (value: number) => {
-      if (!value) {
-        return 0;
-      }
-      return DateManager.humanize(value, router.locale as string);
-    };
-  }, [router.locale]);
+    return getDuration(value, router.locale as string);
+  }, [router.locale, value, getDuration]);
   return (
     <div
       className={classNames("text-xxs uppercase font-semibold px-2 rounded-3xl inline-block text-gray-500 text-nowrap", {
@@ -25,7 +24,7 @@ export default function DurationBadge({ value, type = "gray" }: DurationBadgePro
         "border border-gray-200": type === "bordered",
       })}
     >
-      {duration(value)}
+      {duration}
     </div>
   );
 }
