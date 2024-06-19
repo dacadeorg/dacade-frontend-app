cb1f25ff3eb55d471b0805f456b0a9e6e554141b
mhenriette <mhenriette00@gmail.com>
2024-06-19 12:39:18 +0200

    fix: adjust course material card

--- src/components/badges/durationBadge.tsx
+++ src/components/badges/durationBadge.tsx
@@ -3,7 +3,18 @@ import classNames from "classnames";
 import { useRouter } from "next/router";
 import React, { useMemo } from "react";
 
-export default function DurationBadge({ value, type = "gray" }: { value: number; type?: string }) {
+interface DuratetionBadgeProps{
+  value:number;
+  type?:string;
+}
+
+/**
+ * DurationBadge component
+ *
+ * @param {DuratetionBadgeProps} props - The props for the DurationBadge component.
+ * @returns {JSX.Element} The Learning component JSX element.
+ */
+export default function DurationBadge({ value, type = "gray" }: DuratetionBadgeProps) {
   const router = useRouter();
   const duration = useMemo(() => {
     return (value: number) => {
@@ -14,13 +25,13 @@ export default function DurationBadge({ value, type = "gray" }: { value: number;
     };
   }, [router.locale]);
   return (
-    <span
-      className={classNames("text-xxs uppercase font-semibold px-2 rounded-3xl inline-block text-gray-500", {
+    <div
+      className={classNames("text-xxs uppercase font-semibold px-2 rounded-3xl inline-block text-gray-500 text-nowrap", {
         "bg-gray-200": type === "gray",
         "border border-gray-200": type === "bordered",
       })}
     >
       {duration(value)}
-    </span>
+    </div>
   );
 }
