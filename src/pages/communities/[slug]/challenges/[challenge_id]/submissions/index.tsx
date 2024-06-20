d221eb37a400024adfc732c3dacb4d62a7ad7633
Yannick Musafiri <musayann@gmail.com>
2024-06-20 16:21:13 +0200

    chore(troubleshoot): reduce components used on submission list page for testing

--- src/pages/communities/[slug]/challenges/[challenge_id]/submissions/index.tsx
+++ src/pages/communities/[slug]/challenges/[challenge_id]/submissions/index.tsx
@@ -1,9 +1,4 @@
 import DefaultLayout from "@/components/layout/Default";
-import SubmissionPopup from "@/components/popups/submission";
-import Header from "@/components/sections/communities/_partials/Header";
-import Wrapper from "@/components/sections/courses/Wrapper";
-import SubmissionList from "@/components/sections/submissions/List";
-import MetaData from "@/components/ui/MetaData";
 import useSubmissionNavigation from "@/hooks/useSubmissionNavigation";
 import { wrapper } from "@/store";
 import { fetchAllSubmission, fetchChallenge } from "@/store/services/communities/challenges";
@@ -12,10 +7,10 @@ import { Submission as SubmissionType } from "@/types/bounty";
 import { Community } from "@/types/community";
 import { Challenge } from "@/types/course";
 import { NotFoundError } from "@/utilities/errors/NotFoundError";
+// import { he } from "date-fns/locale";÷
 import { GetServerSideProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Head from "next/head";
 import { ReactElement, useMemo } from "react";
 
 /**
@@ -41,19 +36,21 @@ export default function Submission(props: { pageProps: { currentCommunity: Commu
 
   if (!submissions) return <></>;
 
+  console.log(submissions, challenge, selectedSubmission, handleCloseSubmission, showPopup, headerPaths);
+
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>{`${t("communities.submission.title")} ${challenge?.name}`}</title>
         <MetaData description={challenge?.description as string} />
-      </Head>
-      <Wrapper paths={headerPaths}>
+      </Head> */}
+      {/* <Wrapper paths={headerPaths}> */}
         <div className="flex flex-col py-4 space-y-8 text-gray-700">
-          <Header title={challenge?.name} subtitle={t("communities.submission.title")} isTeamChallenge={challenge?.isTeamChallenge} isHackathon={challenge?.isHackathon} />
-          <SubmissionList />
+          {/* <Header title={challenge?.name} subtitle={t("communities.submission.title")} isTeamChallenge={challenge?.isTeamChallenge} isHackathon={challenge?.isHackathon} /> */}
+          {/* <SubmissionList /> */}
         </div>
-        {showPopup && <SubmissionPopup show={showPopup} onClose={handleCloseSubmission} submissionId={selectedSubmission?.id} />}
-      </Wrapper>
+        {showPopup && "Hello"}
+      {/* </Wrapper> */}
     </>
   );
 }
