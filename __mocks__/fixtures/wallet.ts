0d1e3adde8d413307f0c5ceef064d82b52b64a7e
mhenriette <mhenriette00@gmail.com>
2024-07-26 17:58:34 +0200

    feat: add new test for the walet component

--- __mocks__/fixtures/wallet.ts
+++ __mocks__/fixtures/wallet.ts
@@ -18,7 +18,7 @@ export const mockWallet: Wallet = {
     balance: 0,
     user_id: mockUser.id,
     token: "ICP",
-    address: "",
+    address: "0xd0 5AfA 87A5 99b8 AD8C ff71 b1eC 7129 e3Ed fe08 b9",
     payouts: [mockPayout],
     description: ""
 }
--- __tests__/components/cards/Wallet.test.tsx
+++ __tests__/components/cards/Wallet.test.tsx
@@ -1,22 +1,24 @@
-import CardsWallet from "@/components/cards/Wallet";
+import CardsWallet from "@/components/cards/wallet";
 import "@testing-library/jest-dom";
 import { mockWallet } from "../../../__mocks__/fixtures/wallet";
-import { renderWithRedux } from "../../../__mocks__/renderWithRedux";
 import { screen } from "@testing-library/react";
-
+import { renderWithRedux } from "@__mocks__/renderWithRedux";
 
 // use the actual component when it is done tested
 jest.mock("../../../src/components/sections/profile/modals/EditAddress/index.tsx", () => {
   return <h1>hello</h1>;
 });
 
+jest.mock("@/hooks/useTypedDispatch.ts", () => ({
+  useDispatch: jest.fn(),
+}));
+
 describe("Wallet card component", () => {
-  it("renders the wallet component with all the required elements", () => {
+  it("renders the wallet component with all the children", () => {
     renderWithRedux(<CardsWallet wallet={mockWallet} disabled={false} />);
-    expect(screen.getByTestId("cardWalletId")).toBeInTheDocument()
-    expect(screen.getByText("User wallet")).toBeInTheDocument();
-    expect(screen.getByTestId("tag-value")).toBeInTheDocument();
-    expect(screen.queryAllByTestId("currencyId")).not.toBeNull();
-    expect(screen.getByTestId("coin")).toBeInTheDocument();
+    expect(screen.getByTestId("cardWalletId")).toBeInTheDocument();
+    expect(screen.getByTestId("overviewId")).toBeInTheDocument();
+    expect(screen.getByTestId("cashoutAddressId")).toBeInTheDocument();
+    expect(screen.queryByText("profile.wallet.payout.text")).not.toBeNull();
   });
 });
