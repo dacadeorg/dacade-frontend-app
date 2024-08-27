8312ef0940971046e5a6cdc046a5d60f77fe9554
mhenriette <mhenriette00@gmail.com>
2024-08-27 18:30:33 +0200

    test: add test for Header component

---
+++ __tests__/components/popups/submission/_partials/Header.test.tsx
@@ -0,0 +1,35 @@
+import SubmissionPopup from "@/components/popups/submission/_partials/Header";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+jest.mock("src/icons/arrow-left.svg", () => ({
+  __esModule: true,
+  default: () => <svg data-testid="arrow-left-icon" />,
+}));
+
+
+describe("SubmissionPopup", () => {
+  const mockOnClose = jest.fn();
+
+
+  it("renders the component with correct text", () => {
+    render(<SubmissionPopup onClose={mockOnClose} />);
+    expect(screen.getByText("communities.submission")).toBeInTheDocument();
+  });
+
+  it("calls onClose when clicking the arrow icon", () => {
+    render(<SubmissionPopup onClose={mockOnClose} />);
+
+    const arrowButton = screen.getByText("communities.submission").parentElement;
+    fireEvent.click(arrowButton!);
+    expect(mockOnClose).toHaveBeenCalled();
+  });
+
+  it("calls onClose when clicking the close button", () => {
+    render(<SubmissionPopup onClose={mockOnClose} />);
+    const closeButton = screen.getByRole("button");
+    fireEvent.click(closeButton);
+    expect(mockOnClose).toHaveBeenCalled();
+  });
+
+});
