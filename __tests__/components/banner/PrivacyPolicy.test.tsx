17f53bf16c5005ea2a18c6dc67d31191709ceb5a
igorntwari <igorntwari28@gmail.com>
2024-08-28 16:32:19 +0200

    refactor: separate test  cases

--- __tests__/components/banner/PrivacyPolicy.test.tsx
+++ __tests__/components/banner/PrivacyPolicy.test.tsx
@@ -3,42 +3,35 @@ import PrivacyPolicyBanner from "@/components/banner/PrivacyPolicy";
 import { renderWithRedux } from "@__mocks__/renderWithRedux";
 import { fireEvent, screen } from "@testing-library/react";
 
-const mockPush = jest.fn();
-
-jest.mock("next/router", () => ({
-  useRouter: jest.fn(() => ({
-    locale: "en",
-    push: mockPush,
-  })),
-}));
-
-jest.mock("next-i18next", () => ({
-  useTranslation: () => ({
-    t: (key: string) => key === "signup-page.privacy" ? "Translated Privacy Policy" : key,
-  }),
-}));
-
 describe("PrivacyPolicyBanner", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("should render PrivacyPolicy", () => {
-    const { getByTestId } = renderWithRedux(<PrivacyPolicyBanner />, {
+    renderWithRedux(<PrivacyPolicyBanner />, {
       banner: { showCookiePolicy: true },
     });
-    const privacyPolicy = getByTestId("PrivacyPolicy");
+    const privacyPolicy = screen.getByTestId("PrivacyPolicy");
     expect(privacyPolicy).toBeInTheDocument();
   });
 
-  it("should accept the cookies policy and not render PrivacyPolicy after the user clicks on the close button", () => {
-    const { queryByTestId } = renderWithRedux(<PrivacyPolicyBanner />, {
-      banner: { showCookiePolicy: false },
-    });
-    const closeButton = screen.getByTestId("closeButton");
-    expect(closeButton).toBeInTheDocument();
-    fireEvent.click(closeButton);
-    expect(queryByTestId("PrivacyPolicy")).not.toBeInTheDocument();
+it("should render the close button", () => {
+  const { getByTestId } = renderWithRedux(<PrivacyPolicyBanner />, {
+    banner: { showCookiePolicy: false },
   });
+  const closeButton = getByTestId("closeButton");
+  expect(closeButton).toBeInTheDocument();
+});
+
+it("should not render PrivacyPolicy after the user clicks on the close button", () => {
+  const { queryByTestId } = renderWithRedux(<PrivacyPolicyBanner />, {
+    banner: { showCookiePolicy: false },
+  });
+  const closeButton = screen.getByTestId("closeButton");
+  fireEvent.click(closeButton);
+  expect(queryByTestId("PrivacyPolicy")).not.toBeInTheDocument();
+});
+
   
 });
